9f86fc142ab1bc0c1e64a1bb298ed83714fa3173
pinealan <achan961117@gmail.com>
2018-01-06 03:21:32 +0800

    Implement signatures for API auth

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import hashlib
 API_SECRET = ''
 API_KEY = 'de504dc5763aeef9ff52'
 
+def sign(nonce, customer_id, api_key):
+    message = nonce + customer_id + api_key
+    signature = hmac.new(
+            API_SECRET,
+            msg=message.encode('utf-8'),
+            digestmod=hashlib.sha256
+    ).hexdigest().upper()
+    return signature
+
 import numpy as np
 import pandas as pd
 import requests as req
