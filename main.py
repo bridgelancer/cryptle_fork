589f2b83680674ba196dd9f3dd37fff110775cd6
pinealan <achan961117@gmail.com>
2018-01-06 03:28:31 +0800

    Rearrange library/functions layout

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import hmac
-import hashlib
-
 API_SECRET = ''
 API_KEY = 'de504dc5763aeef9ff52'
+
+import numpy as np
+import pandas as pd
+import requests as req
+import json
+import pysher
+
+import hmac
+import hashlib
 
 def sign(nonce, customer_id, api_key):
     message = nonce + customer_id + api_key
@@ -12,12 +18,6 @@ def sign(nonce, customer_id, api_key):
             digestmod=hashlib.sha256
     ).hexdigest().upper()
     return signature
-
-import numpy as np
-import pandas as pd
-import requests as req
-import json
-import pysher
 
 def bitstamp_socket(channel, event, callback):
     pusher = pysher.Pusher(APP_KEY)
