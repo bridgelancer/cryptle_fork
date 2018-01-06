0667b7f673df89a8ed07ed174e827e422e5d98d6
bridgelancer <thchoy@hotmail.com>
2018-01-07 03:18:04 +0800

    Delete the talib, implemented in-house ATR

--- cryptle.py
+++ cryptle.py
@@ -4,7 +4,6 @@ import pandas as pd
 import requests as req
 from datetime import date
 from time import time
-from talib import ATR
 
 # @stripped, Trading pairs info
 # Assume there is signature and API_key
@@ -21,7 +20,8 @@ def detect_entry_signal(price_window_5, price_window_8):
     Compute high and low for breakout price
     """
     if price_window_5.avg > price_window_8.avg:
-        #limit buy (maybe market price + 0.3%?)
+        # Limit buy (maybe market price + 0.3%?)
+        # PLACEHOLDER
         res = request.post('https://www.bitstamp.net/api/v2/buy/xxxxxx/', param = {'key': '', 'signature': '', 'nonce': '', 'amount': '', 'price': price * 1.003, 'limit_price': price * 0.98})
         
         
@@ -34,20 +34,24 @@ def compute_exit_signal(price_window_5, price_window_8):
     for item in res.json(): # check user transactions
         order_id = item['order_id'] # this needs to be revised
         if order_id == id and live_min_price_5.avg < live_min_price_8.avg:
-            #Market sell
-            res = request.post('https://www.bitstamp.net/api/v2/sell/market/xxxxxx/', param = {'key': '', 'signature': '', 'nonce': '', 'amount': ''}) #amount should be equivalent to the amount bought
+            # Market sell
+            # PLACEHOLDER
+            res = request.post('https://www.bitstamp.net/api/v2/sell/market/xxxxxx/', param = {'key': '', 'signature': '', 'nonce': '', 'amount': ''}) # amount should be equivalent to the amount bought
             
-# taking time period as 5 currently
 def compute_average_true_ranges(price_window):
     """
     Compute ATR, aka N
     """
-   return ATR(
-        price_window.high, #should be a list with 2 elements instead
-        price_window.low, #should be a list with 2 elements instead
-        price_window.close, #should be a list with 2 elements instead
-        timeperiod= 5
-    )
+    # taking time period as 5 currently
+    timeperiod = 5
+
+    true_high = max(price_window.high, price_window.close)
+    true_low = min(price_window.low, price_window.close)
+
+    TR = true_high - true_low
+    TR_p = price_window.high_p - price_window.low_p
+
+    return (TR + TR_p * (timeperiod - 1)) / timeperiod
 
 
 def compute_trade_sizes(price_window):
