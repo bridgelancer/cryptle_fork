17e6332e32b1d74c69e5b2b6b9532176f81950a6
pinealan <achan961117@gmail.com>
2018-01-09 19:01:16 +0800

    Move MA and Candles into ta file/library

---
+++ ta.py
@@ -0,0 +1,127 @@
+import time
+
+
+class MovingWindow:
+
+    # window is the number of seconds in the lookback window
+    # Ticker (optional) is meta-info about what series is being tracked
+    def __init__(self, window, ticker=None):
+        self._ticks = [] # @OPTIMIZE use NumPy arrays
+
+        self.avg = 0
+        self.volume = 0
+        self.dollar_volume = 0
+
+        self.window = window
+        self.ticker = ticker
+
+
+    def __str__(self):
+        return self.ticker
+
+
+    def update(self, price, volume, timestamp=None):
+
+        if timestamp == None:
+            timestamp = time.time()
+
+        self.last = price
+        self.volume = self.volume + volume
+        self.dollar_volume = self.dollar_volume + price * volume
+        self.avg = self.dollar_volume / self.volume
+
+        self._ticks.append((price, volume, timestamp))
+        self.clear()
+
+    def clear(self):
+        now = time.time()
+        lookback = now - self.window
+
+        while True:
+            if self._ticks[0][2] < lookback:
+                tick = self._ticks.pop(0)
+                self.volume = self.volume - tick[1]
+                self.dollar_volume = self.dollar_volume - tick[0] * tick[1]
+                self.avg = self.dollar_volume / self.volume
+            else:
+                break
+
+
+    def high(self):
+        return max(self._ticks)
+
+
+    def low(self):
+        return min(self._ticks)
+
+
+
+class CandleBar:
+    # _bars: List of (open, close, high, low, nth minute bar)
+    # This class is for storing the min-by-min bars the minute before the current tick
+    # default bar size is 1 minute
+    def __init__(self, period=60):
+        self._bars = []
+
+        self.period = period
+        self.lookback = 100
+        self.last = 0
+
+        self.ls = []
+        self.atr_val = 0
+
+        self.barmin = None
+        self.barmax = None
+        self.baropen = None
+        self.barclose = None
+        self.timestamp_last = None
+
+    def update(self, price, timestamp=None):
+
+        if timestamp == None:
+            timestamp = time.time()
+        # execute the following block if
+        # - the current trade happens 60s after the last trade OR
+        # - the current trade falls in the following 60s window
+        if self.timestamp_last == None:
+            self.barmin = self.barmax = self.baropen = self.barclose = price
+            self.timestamp_last = timestamp
+        elif int(timestamp / self.period) != int(self.timestamp_last / self.period):
+            self._bars.append([self.baropen, self.barclose, self.barmax, self.barmin, int(timestamp/self.period) + 1])
+            logger.debug(self._bars[-1])
+            self.timestamp_last = timestamp
+
+            self.barmin = self.barmax = self.baropen = self.barclose = price
+        elif int(timestamp / self.period) == int(self.timestamp_last / self.period):
+            self.barmin = min(self.barmin, price)
+            self.barmax = max(self.barmax, price)
+            self.barclose = price
+
+        self.last = price  # update the current tick prcie
+
+        # @HARDCODE
+        self.compute_atr(5)
+        self.prune(self.lookback)
+
+
+    def compute_atr(self, mins):
+        if (len(self._bars) <= mins and len(self._bars) > 0):
+            self.ls.append(self._bars[-1][2] - self._bars[-1][3])
+            self.atr_var = sum(self.ls) / len(self.ls)
+        elif(len(self._bars) > mins):
+            self.ls.clear()
+            TR = self._bars[-1][2] - self._bars[-1][3]
+            self.atr_val = (self.atr_val * (mins - 1) + TR) / mins
+
+    def get_atr(self):
+
+        #@HARDCODE
+        if (len(self._bars) >= 5):
+            return self.atr_val
+        else:
+            raise RuntimeWarning("ATR not yet available")
+
+    def prune(self, lookback): #discard the inital bars after 100 periods of bar data
+        if len(self._bars) != 0:
+            self._bars = self._bars[-min(len(self._bars), lookback):]
+
