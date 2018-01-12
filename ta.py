c9b24e5cab82c2a1cfc0a730c86db57c2443eb1b
pinealan <achan961117@gmail.com>
2018-01-12 09:34:04 +0000

    Refactor underscored member variables in ta.py

--- ta.py
+++ ta.py
@@ -3,7 +3,7 @@ class MovingWindow:
     # window is the number of seconds in the lookback window
     # Ticker (optional) is meta-info about what series is being tracked
     def __init__(self, period):
-        self._ticks = []
+        self.ticks = []
         self.avg = 0
         self.volume = 0
         self.dollar_volume = 0
@@ -15,7 +15,7 @@ class MovingWindow:
 
         # @THROW @HANDLE
         try:
-            assert timestamp > self._ticks[0][2]
+            assert timestamp > self.ticks[0][2]
         except AssertionError:
             return
         except IndexError:
@@ -27,17 +27,17 @@ class MovingWindow:
         self.dollar_volume += price * volume
         self.avg = self.dollar_volume / self.volume
 
-        self._ticks.append((price, volume, timestamp))
+        self.ticks.append((price, volume, timestamp))
         self.clear()
 
 
     def clear(self):
-        now = self._ticks[-1][2]
+        now = self.ticks[-1][2]
         lookback = now - self.period
 
         while True:
-            if self._ticks[0][2] <= lookback:
-                tick = self._ticks.pop(0)
+            if self.ticks[0][2] <= lookback:
+                tick = self.ticks.pop(0)
 
                 self.volume -= tick[1]
                 self.dollar_volume -= tick[0] * tick[1]
@@ -46,22 +46,22 @@ class MovingWindow:
                 break
 
     def high(self):
-        return max(self._ticks)
+        return max(self.ticks)
 
 
     def low(self):
-        return min(self._ticks)
+        return min(self.ticks)
 
 
 
 class CandleBar:
 
-    # _bars: List of (open, close, high, low, nth minute bar)
+    # bars: List of (open, close, high, low, nth minute bar)
     # This class is for storing the min-by-min bars the minute before the current tick
     # default bar size is 1 minute
     def __init__(self, period=60, scope=5, atr_lookback=5):
-        self._bars = []
-        self._WMAs = []
+        self.bars = []
+        self.WMAs = []
 
         self.period = period
         self.lookback = 100
@@ -87,7 +87,7 @@ class CandleBar:
             self.timestamp_last = timestamp
 
         elif int(timestamp / self.period) != int(self.timestamp_last / self.period):
-            self._bars.append([self.baropen, self.barclose, self.barmax, self.barmin, int(timestamp/self.period) + 1])
+            self.bars.append([self.baropen, self.barclose, self.barmax, self.barmin, int(timestamp/self.period) + 1])
 
             self.barmin = self.barmax = self.baropen = self.barclose = price
             self.timestamp_last = timestamp
@@ -104,36 +104,36 @@ class CandleBar:
 
 
     def computeAtr(self):
-        if (len(self._bars) <= self.atr_lookback):
-            self.ls.append(self._bars[-1][2] - self._bars[-1][3])
+        if (len(self.bars) <= self.atr_lookback):
+            self.ls.append(self.bars[-1][2] - self.bars[-1][3])
             self.atr_val = sum(self.ls) / len(self.ls)
-        elif(len(self._bars) > self.atr_lookback):
+        elif(len(self.bars) > self.atr_lookback):
             self.ls.clear()
-            TR = self._bars[-1][2] - self._bars[-1][3]
+            TR = self.bars[-1][2] - self.bars[-1][3]
             self.atr_val = (self.atr_val * (self.atr_lookback- 1) + TR) / self.atr_lookback
 
 
     def getAtr(self):
 
-        if (len(self._bars) >= self.atr_lookback):
+        if (len(self.bars) >= self.atr_lookback):
             return self.atr_val
         else:
             raise RuntimeWarning("ATR not yet available")
 
 
     def prune(self, lookback): #discard the inital bars after 100 periods of bar data
-        if len(self._bars) != 0:
-            self._bars = self._bars[-min(len(self._bars), lookback):]
+        if len(self.bars) != 0:
+            self.bars = self.bars[-min(len(self.bars), lookback):]
 
 
     def computeWMA(self,open_p = True):
 
-        if len(self._bars) < (self.scope-1):
+        if len(self.bars) < (self.scope-1):
             pass
-        
+
         else:
             price_list = []
-            bar_list = self._bars[-(self.scope - 1):]
+            bar_list = self.bars[-(self.scope - 1):]
 
             if open_p == True:
                 price_list = [x[0] for x in bar_list]
@@ -146,11 +146,11 @@ class CandleBar:
             self.price_list_test = price_list
             weight = list(map(lambda x: x/sum(sequence), sequence))
             self.WMA = sum(p * w for p,w in zip(price_list, weight))
-            self._WMAs.append(self.WMA)
+            self.WMAs.append(self.WMA)
 
 
 
-                
+
 
 
 
