bb6482794b4b4343dc2471161f85e38fe76321c6
bridgelancer <thchoy@hotmail.com>
2018-01-12 20:48:15 +0800

    Data snooping (Changing bar length)

--- strategy.py
+++ strategy.py
@@ -277,7 +277,7 @@ class ATRStrat(Strategy):
 
 class WMAStrat(Strategy):
 
-    def __init__(self, pair, portfolio, message='[ATR]', period=60, scope1=5, scope2=8):
+    def __init__(self, pair, portfolio, message='[ATR]', period=180, scope1=5, scope2=8):
         super().__init__(pair, portfolio)
         self.five_min_bar = CandleBar(period, scope1) # not yet implemented
         self.eight_min_bar = CandleBar(period, scope2) # not yet implemented
@@ -337,6 +337,82 @@ class WMAStrat(Strategy):
         self.prev_sell_time = prev_sell_time
 
 class WMAModStrat(Strategy):
+
+    def __init__(self, pair, portfolio, message='[ATR]', period=180, scope1=5, scope2=8):
+        super().__init__(pair, portfolio)
+        self.five_min_bar = CandleBar(period, scope1)
+        self.eight_min_bar = CandleBar(period, scope2)
+        self.message = message
+
+        self.upper_atr = 0.35
+        self.lower_atr = 0.35
+        self.can_sell = False
+        self.entry_time = None
+
+
+    def __call__(self, tick):
+        price, volume, timestamp = self.unpackTick(tick)
+
+        self.five_min_bar.update(price, timestamp)
+        self.eight_min_bar.update(price, timestamp)
+
+        prev_crossover_time = self.prev_crossover_time
+        prev_sell_time = self.prev_sell_time
+        entry_time = self.entry_time
+        can_sell = self.can_sell
+
+        try:
+            atr = self.five_min_bar.getAtr()
+            belowatr = self.five_min_bar.WMA < price - self.lower_atr * atr
+            aboveatr = min(self.five_min_bar.WMA, self.eight_min_bar.WMA) > price + self.upper_atr * atr
+        except RuntimeWarning:
+            return
+
+        uptrend = self.five_min_bar.WMA > self.eight_min_bar.WMA
+        downtrend = self.five_min_bar.WMA < self.eight_min_bar.WMA
+
+        # @HARDCODE Buy/Sell message
+        if self.hasCash() and not self.hasBalance() and belowatr:
+            #logger.debug('ATR identified uptrend and below ATR band')
+            if prev_crossover_time is None:
+                prev_crossover_time = timestamp
+
+            elif timestamp - prev_crossover_time >= self.timelag_required:
+
+                amount = self.equity_at_risk * self.equity() / price
+                self.buy(amount, price, timestamp, self.message)
+
+                prev_crossover_time = None
+
+                if uptrend:
+                    can_sell = True
+                elif downtrend:
+                    can_sell = False
+                entry_time = timestamp
+
+        elif self.hasBalance():
+            if not can_sell and uptrend:
+                can_sell = True
+            elif can_sell and downtrend:
+                # logger.debug('ATR identified downtrend')
+                if prev_crossover_time is None:
+                    prev_crossover_time = timestamp
+
+                elif timestamp - prev_crossover_time >= self.timelag_required:
+                    self.sellAll(price, timestamp, self.message)
+                    prev_crossover_time = None
+                    prev_sell_time = timestamp
+                    entry_time = None
+
+        else:
+            prev_crossover_time = None
+
+        self.prev_crossover_time = prev_crossover_time
+        self.prev_sell_time = prev_sell_time
+        self.entry_time = entry_time
+        self.can_sell = can_sell
+
+class WMADiscreteStrat(Strategy):
 
     def __init__(self, pair, portfolio, message='[ATR]', period=60, scope1=5, scope2=8):
         super().__init__(pair, portfolio)
