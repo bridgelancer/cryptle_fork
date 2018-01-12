bc57efc3f76ecfa5aa41a69f14939a5df5edab01
pinealan <achan961117@gmail.com>
2018-01-12 12:14:30 +0000

    Extract buy/sell from Strategy to "Exchange" interface

---
+++ exchange.py
@@ -0,0 +1,41 @@
+import logging
+
+
+log = logging.getlog('Exchange')
+
+
+class PaperExchange:
+
+    def marketBuy(self, pair, amount):
+        assert isinstance(pair, str)
+        assert amount > 0
+
+        log.info('Buy  {:>7.2f} {} @${:.2} {}'.format(amount, pair.upper(), price))
+        return {'price': price, 'amount': amount}
+
+
+    def marketSell(self, pair, amount):
+        assert isinstance(pair, str)
+        assert amount > 0
+
+        log.info('Sell {:>7.2f} {} @${:.2f} {}'.format(amount, pair.upper(), price))
+        return {'price': price, 'amount': amount}
+
+    def limitBuy(self, pair, amount, price):
+        assert isinstance(pair, str)
+        assert amount > 0
+        assert price > 0
+
+        log.info('Buy  {:>7.2f} {} @${:.2} {}'.format(amount, pair.upper(), price))
+        return {'price': price, 'amount': amount}
+
+
+    def limitSell(self, pair, amount, price):
+        assert isinstance(pair, str)
+        assert amount > 0
+        assert price > 0
+
+        log.info('Sell {:>7.2f} {} @${:.2f} {}'.format(amount, pair.upper(), price))
+        return {'price': price, 'amount': amount}
+
+
