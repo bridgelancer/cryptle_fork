cc2b9ee2d887e566a8a39f50f6dcc8ed0e5c8726
pinealan <achan961117@gmail.com>
2018-01-16 21:51:42 +0000

    Add support for Bitstamp Socket API for orderbook

--- datafeed.py
+++ datafeed.py
@@ -31,6 +31,46 @@ class BitstampFeed:
             self._bindSocket('live_orders_' + pair, 'order_created', callback)
 
 
+    def onOrderChanged(self, pair, callback):
+        assert isinstance(pair, str)
+        assert callable(callback)
+
+        if pair == 'btcusd':
+            self._bindSocket('live_orders', 'order_changed', callback)
+        else:
+            self._bindSocket('live_orders_' + pair, 'order_changed', callback)
+
+
+    def onOrderDeleted(self, pair, callback):
+        assert isinstance(pair, str)
+        assert callable(callback)
+
+        if pair == 'btcusd':
+            self._bindSocket('live_orders', 'order_deleted', callback)
+        else:
+            self._bindSocket('live_orders_' + pair, 'order_deleted', callback)
+
+
+    def onOrderBookUpdate(self, pair, callback):
+        assert isinstance(pair, str)
+        assert callable(callback)
+
+        if pair == 'btcusd':
+            self._bindSocket('order_book', 'data', callback)
+        else:
+            self._bindSocket('order_book_' + pair, 'data', callback)
+
+
+    def onOrderBookDiff(self, pair, callback):
+        assert isinstance(pair, str)
+        assert callable(callback)
+
+        if pair == 'btcusd':
+            self._bindSocket('diff_order_book', 'data', callback)
+        else:
+            self._bindSocket('diff_order_book_' + pair, 'data', callback)
+
+
     def _bindSocket(self, channel_name, event, callback):
 
         if channel_name not in self.pusher.channels:
