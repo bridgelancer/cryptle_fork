f5191f509456025a82bafa905b09627a35f5e8cd
pinealan <achan961117@gmail.com>
2018-01-20 15:52:32 +0000

    Add reference to loglevel.py for inbuilt log levels

--- loglevel.py
+++ loglevel.py
@@ -1,5 +1,12 @@
 import logging
 
+# CRITICAL = 50
+# ERROR    = 40
+# WARNING  = 30
+# INFO     = 20
+# DEBUG    = 10
+# NOTSET   = 0
+
 logging.TICK    = 9
 logging.TA      = 9
 logging.REPORT  = 21
