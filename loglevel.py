319bf97ed94ad0c95a28b6efd3f19d6b09b14f65
pinealan <achan961117@gmail.com>
2018-01-20 14:31:39 +0000

    Define new log levels in separate file

---
+++ loglevel.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.TICK    = 9
+logging.TA      = 9
+logging.REPORT  = 21
+
+logging.addLevelName(logging.TICK, 'TICK')
+logging.addLevelName(logging.TA, 'TA')
+logging.addLevelName(logging.REPORT, 'REPORT')
+
