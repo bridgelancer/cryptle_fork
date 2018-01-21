d265c22fdce4b1915009790b2623cf59bc80c62e
pinealan <achan961117@gmail.com>
2018-01-21 20:09:04 +0000

    Tweak loglevel

--- loglevel.py
+++ loglevel.py
@@ -8,25 +8,25 @@ import logging
 # NOTSET   = 0
 
 # Define new log levels
-logging.SIGNAL  = 27
 logging.REPORT  = 25
+logging.SIGNAL  = 23
 logging.INDEX   = 7
 logging.TICK    = 5
 
-logging.addLevelName(logging.SIGNAL, 'SIGNAL')
 logging.addLevelName(logging.REPORT, 'REPORT')
+logging.addLevelName(logging.SIGNAL, 'SIGNAL')
 logging.addLevelName(logging.INDEX, 'INDEX')
 logging.addLevelName(logging.TICK, 'TICK')
 
 
 # Add convenience functions to the default Logger and it's children instances
-def signal(self, message, *args, **kargs):
-    if self.isEnabledFor(logging.SIGNAL):
-        self._log(logging.SIGNAL, message, args, **kargs)
-
 def report(self, message, *args, **kargs):
     if self.isEnabledFor(logging.REPORT):
         self._log(logging.REPORT, message, args, **kargs)
+
+def signal(self, message, *args, **kargs):
+    if self.isEnabledFor(logging.SIGNAL):
+        self._log(logging.SIGNAL, message, args, **kargs)
 
 def index(self, message, *args, **kargs):
     if self.isEnabledFor(logging.INDEX):
@@ -36,8 +36,8 @@ def tick(self, message, *args, **kargs):
     if self.isEnabledFor(logging.TICK):
         self._log(logging.TICK, message, args, **kargs)
 
-logging.Logger.signal = signal
 logging.Logger.report = report
+logging.Logger.signal = signal
 logging.Logger.index  = index
 logging.Logger.tick   = tick
 
