5e866c5f731b7d32e74ecf9801527005a7d728bf
pinealan <achan961117@gmail.com>
2018-01-21 04:46:30 +0000

    Extend logging functions for Logger base class
    - Define multiple logging levels at for common
    - Attach convenience functions to the base logger in logging.Logger

--- loglevel.py
+++ loglevel.py
@@ -7,11 +7,35 @@ import logging
 # DEBUG    = 10
 # NOTSET   = 0
 
-logging.TICK    = 9
-logging.TA      = 9
-logging.REPORT  = 21
+logging.SIGNAL  = 27
+logging.REPORT  = 25
+logging.INDEX   = 7
+logging.TICK    = 5
 
-logging.addLevelName(logging.TICK, 'TICK')
-logging.addLevelName(logging.TA, 'TA')
+logging.addLevelName(logging.SIGNAL, 'SIGNAL')
 logging.addLevelName(logging.REPORT, 'REPORT')
+logging.addLevelName(logging.INDEX, 'INDEX')
+logging.addLevelName(logging.TICK, 'TICK')
+
+
+def signal(self, message, *args, **kargs):
+    if self.isEnabledFor(logging.SIGNAL):
+        self._log(logging.SIGNAL, message, args, **kws)
+
+def report(self, message, *args, **kws):
+    if self.isEnabledFor(logging.REPORT):
+        self._log(logging.REPORT, message, args, **kws)
+
+def index(self, message, *args, **kargs):
+    if self.isEnabledFor(logging.INDEX):
+        self._log(logging.INDEX, message, args, **kws)
+
+def tick(self, message, *args, **kargs):
+    if self.isEnabledFor(logging.TICK):
+        self._log(logging.TICK, message, args, **kws)
+
+logging.Logger.signal = signal
+logging.Logger.report = report
+logging.Logger.index  = index
+logging.Logger.tick   = tick
 
