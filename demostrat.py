7935a67d63b3ed9d71f39782a8fdb009fe5a122b
pinealan <achan961117@gmail.com>
2018-01-26 03:06:14 +0000

    Add demo file of new infrastructure usage

---
+++ demostrat.py
@@ -0,0 +1,65 @@
+from cryptle.strategy import *
+from cryptle.loglevel import *
+from cryptle.backtest import *
+from plotting import *
+from ta import *
+
+import logging
+import sys
+import matplotlib.pyplot as plt
+
+class DemoStrat(Strategy):
+
+    def __init__(s, period=180, wma1_lb=5, wma2_lb=8, **kws):
+        # **kws needs to include:
+        # - pair
+        # - portfolio
+        # - exchange
+
+        s.indicators = {}
+        s.indicators['vwma'] = ContinuousVWMA(300)
+        s.indicators['candle'] = candle = CandleBar(period)
+        s.wma1 = WMA(candle, wma1_lb)
+        s.wma2 = WMA(candle, wma2_lb)
+
+        s.buy_signal = False
+        s.sell_signal = False
+        s.entered = False
+
+        # !Important! This line needs to be at the end of __init__
+        super().__init__(**kws)
+
+
+    def generateSignal(s, price, timestamp, volume, action):
+        if s.vwma < 0:
+            s.buy_signal = True
+            s.sell_signal = False
+        elif s.vwma > 0:
+            s.buy_signal = False
+            s.sell_signal = True
+
+
+    def execute(s):
+        if s.hasCash() and not s.entered and s.buy_signal:
+            s.marketBuy(s.maxBuyAmount())
+            return
+
+        if s.entered and s.sell_signal:
+            s.marketSell(s.maxSellAmount())
+            return
+
+
+if __name__ == '__main__':
+    pair = 'btcusd'
+    port = Portfolio(10000)
+    exchange = PaperExchange(commission=0.0012)
+
+    strat = DemoStrat(pair=pair, portfolio=port, exchange=exchange)
+
+    test = Backtest(exchange)
+    test.readJSON('../../../../data/bitstamp/btc.02.log')
+    test.run(strat.tick)
+
+    plotCandles(strat.candle)
+    plt.show()
+
