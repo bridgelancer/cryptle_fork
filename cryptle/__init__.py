94bbed3b4ac720d42fd17a1eeaee6674dc6137ae
pinealan <achan961117@gmail.com>
2018-01-27 06:41:05 +0000

    Avoid full import of cryptle modules to reduce dependency

--- cryptle/__init__.py
+++ cryptle/__init__.py
@@ -1,9 +1,5 @@
-from cryptle.backtest import *
-from cryptle.datafeed import *
-from cryptle.exchange import *
 from cryptle.strategy import *
 from cryptle.loglevel import *
 from cryptle.utility  import *
 
 __all__ = ['datafeed', 'exchange', 'strategy', 'loglevel', 'utility', 'backtest']
-
