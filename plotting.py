38fe9e4944d72f46c394de6e1a76b998fb1c9fc2
pinealan <achan961117@gmail.com>
2018-01-29 01:17:29 +0000

    Overhaul plotting library with backwards compatibility
    - Adopted an OO interface for the charting implemention
    - Compatibiilty maintained with wrapper function

--- plotting.py
+++ plotting.py
@@ -1,137 +1,150 @@
 from datetime import datetime
-
 import matplotlib.pyplot as plt
 
-def plotCandles(candle, title=None, technicals=None, trades=None, plot_volume=False, volume_title=None,
-        volume_scale=None):
-    # Args:
-    #   candledata:
-    #       A filled Candlebar
-    #
-    #   title:
-    #       An optional title for the chart
-    #
-    #   plot_volume:
-    #       If True, plots volume bars
-    #
-    #   color_function:
-    #       A function which, given a row index and price series, returns a candle color.
-    #
-    #   technicals:
-    #       A list of additional data series to add to the chart.  Must be the same length as pricing.
-    #
-    #   trades:
-    #       A list of tuples in the format (entry_time, entry_price, exit_time, exit_price)
-    #       the entry and exit times should be in unix timestamp
-
-    technicals = technicals or []
-    trades = trades or []
-
-    if plot_volume:
-        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3,1]})
-    else:
-        fig, ax1 = plt.subplots(1, 1, figsize=(16,12))
-
-    if title:
-        ax1.set_title(title)
+# @Temporary wrapper for transitioning
+def plotCandles(*args, **kws):
+    plot(*args, **kws)
 
 
-    # Plot the bars and lines through bars
-    for i, bar in enumerate(candle):
-        op = bar[0] # open
-        cl = bar[1] # close
-        hi = bar[2] # hi
-        lo = bar[3] # low
+def plot(candle=None,
+        title=None,
+        trades=None,
+        signals=[],
+        indicators=[],
+        plot_volume=False,
+        volume_title=None,
+        fig=None):
+    '''Wrapper function for OO interface of CandleStickChart
 
-        barlen = abs(op - cl)
-        bottom = min(op, cl)
-        color = 'r' if op > cl else 'g'
-        fill  = True if color =='r' else False
+    Args:
+        candledata: A filled Candlebar
+        title: An optional title for the chart
+        signals: Sequence of metrics to be plotted over (on top of) the candlestick chart
+        indicators: Sequence of metrics to plotted below the candlestick chart in subplots
+        trades: A list of tuples in the following format
+            (entry_time, entry_price, exit_time, exit_price)
+        plot_volume: If True, plot volume bars
+    '''
+    numplots = 1 + len(signals) + len(indicators) + plot_volume
+    chart = CandleStickChart(numplots, title)
 
-        ax1.bar(i, barlen, bottom=bottom, color=color, edgecolor=color, fill=fill, linewidth=1)
-        ax1.vlines(i, lo, hi, color=color, linewidth=1)
+    chart.plotCandle(candle, plot_volume)
+    chart.plotTrade(trades)
+    for sign in signals:
+        chart.plotSignal(sign)
+    for index in indicators:
+        chart.plotIndicator(index)
 
-    # Plot buy/sells
-    for trade in trades:
-        try:
-            entry = int(trade[0]) # entry time (buy)
-            exit  = int(trade[2]) # exit time (sell)
+    chart.scale()
 
-            # find the corresponding candle where the buy/sell happened
-            entry_bar = int((entry - candle[0][4] * candle.period) / candle.period)
-            exit_bar  = int((exit  - candle[0][4] * candle.period) / candle.period)
-
-            p_and_l = (trade[3] - trade[1])/trade[1]
-
-        except IndexError:
-            # when the strategy terminated without selling (still holding a position)
-            entry = int(trade[0])
-
-            exit_bar  = len(candle)
-            entry_bar = int((entry - candle[0][4] * candle.period) / candle.period)
-
-            p_and_l = (candle.last - trade[1])/trade[1]
-
-        # winning trade colors
-        if p_and_l > 0.005:
-            color = '#97ED8A'
-        if p_and_l > 0.01:
-            color = '#45BF55'
-        if p_and_l > 0.015:
-            color = '#167F39'
-        if p_and_l > 0.02:
-            color = '#044C29'
-
-        # losing trade colors
-        if p_and_l < -0.005:
-            color = '#D40D12'
-        if p_and_l < -0.01:
-            color = '#94090D'
-        if p_and_l < -0.015:
-            color = '#5C0002'
-        if p_and_l < -0.02:
-            color = '#450003'
-
-        ax1.axvspan(entry_bar, exit_bar, facecolor=color, alpha=0.35)
-
-    # Plot indicators
-    for indicator in technicals:
-        ax1.plot(x, indicator)
-
-    # Plots volume
-    if plot_volume:
-        volume = [x[5] for x in candle.bars]
-
-        if volume_scale == None:
-            if max(volume) > 1000000:
-                volume_scale = 1000000
-            elif max(volume) > 1000:
-                volume_scale = 1000
-            else:
-                volume_scale = 1
-
-        if volume_scale >= 1000000:
-            scale = 'M'
-        elif volume_scale >= 1000:
-            scale = 'K'
-        else:
-            scale = ''
-
-        ax2.bar(x, volume, color=candle_colors)
-        volume_title = volume_title or 'Volume'
-
-        if volume_scale:
-            volume_title = 'Volume (%s)' % volume_scale
-
-        ax2.set_title(volume_title)
-        ax2.xaxis.grid(False)
+    return chart
 
 
-    # Set X axis tick labels.
-    time_format = '%m-%d %H:%M'
+class CandleStickChart:
 
-    xlabels = [i * int(len(candle)/7.5) for i in range(8)]
-    dt = [datetime.fromtimestamp(candle[x][4] * candle.period).strftime(time_format) for x in xlabels]
-    plt.xticks(xlabels, dt)
+    def __init__(self, numplots=1, title=None):
+        self._fig , self._axes = plt.subplots(numplots, 1, sharex=True, figsize=(16,12))
+        if numplots == 1:
+            self._axes = [self._axes]
 
-    return fig
+        self._axes[0].set_title(title or 'Plot')
+        self._axes[0].autoscale(enable=False, axis='both')
+        self._axes[0].use_stick_edges = False
+        self._axcounter = 0
+
+
+    # @Refactor Use Candle class interface?
+    def plotCandle(self, candle, plot_volume=False, numxlabels=10):
+        n = len(candle)
+
+        barlen = [abs(bar[0] - bar[1]) for bar in candle]
+        bottom = [min(bar[0], bar[1]) for bar in candle]
+        hi = [bar[2] for bar in candle]
+        lo = [bar[3] for bar in candle]
+        ts = [bar[4] * candle.period for bar in candle]
+
+        color = ['g' if bar[0] < bar[1] else 'r' for bar in candle]
+        fill  = [c == 'r' for c in color]
+
+        self._axes[0].bar(x=ts, height=barlen, bottom=bottom, color=color, edgecolor=color, fill=fill, linewidth=1)
+        self._axes[0].vlines(x=ts, ymin=lo, ymax=hi, color=color, linewidth=1)
+
+        # Plots volume
+        if plot_volume:
+            volume = [x[5] for x in candle.bars]
+            self._axes[1].bar(x, volume, color=candle_colors)
+            volume_title = 'Volume'
+            self._axes[1].set_title(volume_title)
+            self._axes[1].xaxis.grid(False)
+
+        # Set X axis tick labels
+        time_format = '%m-%d %H:%M'
+
+        # Candle timestamps were used as x-axis labels
+        ticks = [candle[x][4] * candle.period for x in range(0, n, int(n / numxlabels))]
+        dates = [datetime.fromtimestamp(t).strftime(time_format) for t in ticks]
+        self._axes[0].set_xticks(ticks)
+        self._axes[0].set_xticklabels(dates, rotation=30)
+
+
+    def plotTrade(self, trades):
+        for trade in trades:
+            try:
+                entry = int(trade[0]) # entry time (buy)
+                exit  = int(trade[2]) # exit time (sell)
+                p_and_l = (trade[3] - trade[1])/trade[1]
+
+            except IndexError:
+                # when the strategy terminated without selling (still holding a position)
+                entry = int(trade[0])
+                exit = self._axes[0].get_xbound()[1]
+                p_and_l = 0
+
+            color = '#ffffad'
+
+            # winning trade colors
+            if p_and_l > 0.005:
+                color = '#97ED8A'
+            if p_and_l > 0.01:
+                color = '#45BF55'
+            if p_and_l > 0.015:
+                color = '#167F39'
+            if p_and_l > 0.02:
+                color = '#044C29'
+
+            # losing trade colors
+            if p_and_l < -0.005:
+                color = '#D40D12'
+            if p_and_l < -0.01:
+                color = '#94090D'
+            if p_and_l < -0.015:
+                color = '#5C0002'
+            if p_and_l < -0.02:
+                color = '#450003'
+
+            self._axes[0].axvspan(entry, exit, facecolor=color, alpha=0.35)
+
+
+    def plotSignal(self, signal):
+        '''Plot metrics over the candlestick chart
+
+        Args:
+            signal: A (2 x N) series; (1, :) are signal values; and (2, :) are unix timestamps
+        '''
+        self._axes[0].plot(signal)
+
+
+    def plotIndicator(self, indicators):
+        '''Plot indicators below the candlestick chart
+
+        Args:
+            signal: A (2 x N) series; (1, :) are indicator values; and (2, :) are unix timestamps
+        '''
+        self._axcounter += 1
+        ax = self._axes[-self._axcounter]
+        ax.plot(indicator)
+
+    def scale(self):
+        for ax in self._axes:
+            ax.use_stick_edges = True
+            ax.autoscale()
