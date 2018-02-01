a822271b358a676253557e1f53ad1abe78e6371c
pinealan <achan961117@gmail.com>
2018-02-01 09:59:43 +0000

    [TWIN] Fuck this I give up

--- twinstrat.py
+++ twinstrat.py
@@ -1,13 +1,18 @@
-from cryptle.strategy import Strategy, IntensityFlag
+from cryptle.strategy import Strategy
 from cryptle.loglevel import *
 from cryptle.utility  import *
 from ta import *
 
+from enum import Enum
 import logging
 logger = logging.getLogger('Cryptle')
 
 
 class TwinStrat(Strategy):
+
+    class State(Enum):
+        ACTIVE  = auto()
+        PASSIVE = auto()
 
     def __init__(s,
             period=120,
@@ -18,10 +23,9 @@ class TwinStrat(Strategy):
             latr=0.5,
             ma1_lb=5,
             ma2_lb=8,
-            macd_lb=5,
+            macd_lb=4,
             rsi_lb=14,
-            stoploss_percent=1,
-            stoploss_cooldown=120,
+            timelag_required=10,
             message='[Twin]',
             **kws):
 
@@ -44,8 +48,8 @@ class TwinStrat(Strategy):
         s.latr = latr
         s.bband = bband
         s.boll_tradable_len = boll_tradable_len
-        s.stoploss_percent = stoploss_percent
-        s.stoploss_cooldown = stoploss_cooldown
+        s.timelag_required = timelag_required
+        s.state = State.PASSIVE
 
         s.prev_buy_price = 0
         s.prev_buy_time = 0
@@ -53,14 +57,12 @@ class TwinStrat(Strategy):
         s.tradable_window = 0
         s.init_time = 0
         s.bought_at_uptrend = False
-        s.stoploss_flag = False
-        s.was_stoploss = False
 
-        s.atr_signal = False
+        s.last_buy_signal = None
         s.macd_signal = False
         s.rsi_sell_flag = False
-        s.rsi_flag = IntensityFlag()
-        s.bollinger_flag = IntensityFlag()
+        s.rsi_signal = False
+        s.bollinger_signal = False
 
 
     def handleTick(s, price, timestamp, volume, action):
@@ -71,45 +73,50 @@ class TwinStrat(Strategy):
         s.signifyBoll(timestamp)
         s.signifyMACD()
         s.signifyRSI()
-        s.signifyStoploss(price)
 
         # Prevent trade if the tick was within the same bar
         if int(timestamp / s.period) == int(s.prev_buy_time / s.period):
             return -1
+        elif int(timestamp / s.period) > int(s.prev_buy_time / s.period):
+            s.prev_buy_time = 0
 
 
     def doneInit(s, timestamp):
         if s.init_time == 0:
             s.init_time = timestamp
-        return timestamp > s.init_time + max(s.wma1.lookback, 10) * s.bar.period
+        return timestamp > s.init_time + max(s.wma1.lookback, 20) * s.bar.period
 
 
     def signifyATR(s, price):
-        s.atr_signal = False
         atr = s.atr.atr
 
         belowatr = max(s.wma1.wma, s.wma2.wma) < price - s.latr * atr
         aboveatr = min(s.wma1.wma, s.wma2.wma) > price + s.uatr * atr
-        s.uptrend   = s.wma1.wma > s.wma2.wma
+        s.uptrend = s.wma1.wma > s.wma2.wma
         s.downtrend = s.wma1.wma < s.wma2.wma
 
-        if belowatr:
-            s.atr_signal = True
-        elif not s.bought_at_uptrend and aboveatr:
-            s.atr_signal = False
-        elif s.bought_at_uptrend and s.downtrend:
-            s.atr_signal = False
-        elif not s.bought_at_uptrend and  s.uptrend:
-            s.bought_at_uptrend = True
+        if not s.hasBalance:
+            if belowatr:
+                s.atr_signal = True
+            else:
+                s.last_buy_signal = None
+
+        elif s.hasBalance:
+            if not s.bought_at_uptrend and aboveatr:
+                s.atr_signal = False
+            elif s.bought_at_uptrend and s.downtrend:
+                s.atr_signal = False
+            elif not s.bought_at_uptrend and s.uptrend:
+                s.bought_at_uptrend = True
 
 
     def signifyBoll(s, timestamp):
         if s.bollinger > s.bband:
-            s.bollinger_flag.setHigh()
+            s.bollinger_signal = True
             s.tradable_window = timestamp
 
         if timestamp > s.tradable_window + s.boll_tradable_len:
-            s.bollinger_flag.setLow()
+            s.bollinger_signal = False
 
 
     def signifyMACD(s):
@@ -120,53 +127,59 @@ class TwinStrat(Strategy):
 
 
     def signifyRSI(s):
-        if s.rsi.rsi > 50:
-            s.rsi_flag.setHigh()
+        if s.rsi > 50:
+            s.rsi_signal = True
 
-        if s.rsi.rsi > 70:
+        if s.rsi > 70:
             s.rsi_sell_flag = True
 
         if s.rsi_sell_flag and s.downtrend:
-            s.rsi_flag.setLow()
+            s.rsi_signal = False
 
         if s.rsi.rsi < 50:
-            s.rsi_flag .setLow()
+            s.rsi_signal = False
             s.rsi_sell_flag = False
 
-
-    def signifyStoploss(s, price):
-        if price <= s.prev_buy_price * (100 - s.stoploss_percent) / 100:
-            s.stoploss_flag = True
+        if s.rsi.rsi < 35:
+            s.rsi_reversion = True
+        else:
+            s.rsi_reversion = False
 
 
     def execute(s, timestamp):
         if s.hasCash and not s.hasBalance:
-            if s.was_stoploss and timestamp < s.stoploss_cooldown + s.prev_sell_time:
-                return
+            if s.rsi_signal and s.bollinger_signal and s.macd_signal:
+                if s.last_buy_signal is None:
+                    s.last_buy_signal = timestamp
 
-            if s.rsi_flag.high and s.bollinger_flag.high and s.macd_signal:
+                elif timestamp >= s.last_buy_signal + s.timelag_required:
+                    s.marketBuy(s.maxBuyAmount)
+                    s.last_buy_signal = None
+                    s.prev_buy_time = timestamp
+                    s.prev_buy_price = s.last_price
+                    s.bought_at_reversion = False
+
+            elif s.rsi_reversion and not s.bollinger_signal:
                 s.marketBuy(s.maxBuyAmount)
                 s.prev_buy_time = timestamp
-                s.prev_buy_price = s.last_price
-                s.bought_at_uptrend = s.uptrend
-                s.bought_at_reversion = False
-                s.was_stoploss = False
-            elif s.rsi_flag.low and s.bollinger_flag.low and s.atr_signal:
-                s.marketBuy(s.maxBuyAmount)
-                s.prev_buy_time = timestamp
-                s.bought_at_uptrend = s.uptrend
                 s.bought_at_reversion = True
 
         elif s.hasBalance:
-            if s.stoploss_flag:
+            if not s.rsi_signal and s.rsi_sell_flag:
                 s.marketSell(s.maxSellAmount)
-                s.was_stoploss = True
+                logger.signal('Sell: Over 70 RSI')
                 s.prev_sell_time = timestamp
-            elif s.rsi_flag.low and not s.bought_at_reversion:
+
+            elif not s.rsi_signal and not s.macd_signal:
+                s.marketSell(s.maxSellAmount)
+                logger.signal('Sell: Normal RSI + MACD')
+                s.prev_sell_time = timestamp
+
+            elif s.bought_at_reversion and s.rsi_sell_flag:
                 s.marketSell(s.maxSellAmount)
                 s.prev_sell_time = timestamp
-            elif s.bought_at_reversion and s.rsi_flag.high and not s.atr_signal:
-                s.marketSell(s.maxSellAmount)
+
+            s.last_buy_signal = None
 
 
 if __name__ == '__main__':
@@ -175,7 +188,7 @@ if __name__ == '__main__':
     from cryptle.plotting import *
     import matplotlib.pyplot as plt
 
-    formatter = defaultFormatter()
+    formatter = defaultFormatter(notimestamp=True)
     fh = logging.FileHandler('twinstrat.log', mode='w')
     fh.setLevel(logging.METRIC)
     fh.setFormatter(formatter)
@@ -185,6 +198,9 @@ if __name__ == '__main__':
     logger.setLevel(logging.METRIC)
     logger.addHandler(sh)
     logger.addHandler(fh)
+    baselog = logging.getLogger('cryptle.strategy')
+    baselog.setLevel(logging.METRIC)
+    baselog.addHandler(fh)
 
     equity = []
 
@@ -200,7 +216,7 @@ if __name__ == '__main__':
 
     strat = TwinStrat(
             period=120,
-            bband=8.0,
+            bband=7.0,
             boll_lb=20,
             boll_tradable_len=3600,
             pair=pair,
