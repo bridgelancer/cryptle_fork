e9cf8e3ac8a521aa58339566c54c623eed7d6565
pinealan <achan961117@gmail.com>
2018-02-05 14:46:50 +0000

    Add test for metric base

--- metric/base.py
+++ metric/base.py
@@ -7,11 +7,26 @@ class Metric:
     def __float__(self):
         return float(self._value)
 
+    def __neg__(self):
+        return -self._value
+
+    def __abs__(self):
+        return abs(self._value)
+
     def __str__(self):
         return str(self._value)
 
     def __repr__(self):
         return str(self._value)
+
+    def __bool__(self):
+        return bool(self._value)
+
+    def __eq__(self, other):
+        return self._value == other
+
+    def __ne__(self, other):
+        return self._value != other
 
     def __lt__(self, other):
         return self._value < other
@@ -28,9 +43,47 @@ class Metric:
     def __add__(self, other):
         return self._value + other
 
+    def __sub__(self, other):
+        return self._value - other
+
+    def __mul__(self, other):
+        return self._value * other
+
+    def __truediv__(self, other):
+        return self._value / other
+
+    def __floordiv__(self, other):
+        return self._value // other
+
+    def __divmod__(self, other):
+        return divmod(self._value, other)
+
+    def __mod__(self, other):
+        return self._value % other
+
+    def __pow__(self, other):
+        return self._value ** other
+
     def __radd__(self, other):
         return other + self._value
 
-    def __iadd__(self, other):
-        self._value += other
-        return self
+    def __rsub__(self, other):
+        return other - self._value
+
+    def __rmul__(self, other):
+        return other * self._value
+
+    def __rtruediv__(self, other):
+        return other / self._value
+
+    def __rfloordiv__(self, other):
+        return other // self._value
+
+    def __rdivmod__(self, other):
+        return divmod(other, self._value)
+
+    def __rmod__(self, other):
+        return other % self._value
+
+    def __rpow__(self, other):
+        return other ** self._value
