37d126608080316f90efa0fcb0efa1063b9f7f0c
pinealan <achan961117@gmail.com>
2018-02-07 21:52:39 +0000

    Fix new CandleMetrics SMA/EMA/WMA

--- metric/base.py
+++ metric/base.py
@@ -2,88 +2,88 @@ class Metric:
     '''Base class with common functions of single valued metrics'''
 
     def __int__(self):
-        return int(self._value)
+        return int(self.value)
 
     def __float__(self):
-        return float(self._value)
+        return float(self.value)
 
     def __neg__(self):
-        return -self._value
+        return -self.value
 
     def __abs__(self):
-        return abs(self._value)
+        return abs(self.value)
 
     def __str__(self):
-        return str(self._value)
+        return str(self.value)
 
     def __repr__(self):
-        return str(self._value)
+        return str(self.value)
 
     def __bool__(self):
-        return bool(self._value)
+        return bool(self.value)
 
     def __eq__(self, other):
-        return self._value == other
+        return self.value == other
 
     def __ne__(self, other):
-        return self._value != other
+        return self.value != other
 
     def __lt__(self, other):
-        return self._value < other
+        return self.value < other
 
     def __gt__(self, other):
-        return self._value > other
+        return self.value > other
 
     def __le__(self, other):
-        return self._value <= other
+        return self.value <= other
 
     def __ge__(self, other):
-        return self._value >= other
+        return self.value >= other
 
     def __add__(self, other):
-        return self._value + other
+        return self.value + other
 
     def __sub__(self, other):
-        return self._value - other
+        return self.value - other
 
     def __mul__(self, other):
-        return self._value * other
+        return self.value * other
 
     def __truediv__(self, other):
-        return self._value / other
+        return self.value / other
 
     def __floordiv__(self, other):
-        return self._value // other
+        return self.value // other
 
     def __divmod__(self, other):
-        return divmod(self._value, other)
+        return divmod(self.value, other)
 
     def __mod__(self, other):
-        return self._value % other
+        return self.value % other
 
     def __pow__(self, other):
-        return self._value ** other
+        return self.value ** other
 
     def __radd__(self, other):
-        return other + self._value
+        return other + self.value
 
     def __rsub__(self, other):
-        return other - self._value
+        return other - self.value
 
     def __rmul__(self, other):
-        return other * self._value
+        return other * self.value
 
     def __rtruediv__(self, other):
-        return other / self._value
+        return other / self.value
 
     def __rfloordiv__(self, other):
-        return other // self._value
+        return other // self.value
 
     def __rdivmod__(self, other):
-        return divmod(other, self._value)
+        return divmod(other, self.value)
 
     def __rmod__(self, other):
-        return other % self._value
+        return other % self.value
 
     def __rpow__(self, other):
-        return other ** self._value
+        return other ** self.value
