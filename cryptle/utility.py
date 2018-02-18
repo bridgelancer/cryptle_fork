f1af713e5273868bba9243d03852361c49c1dcee
pinealan <achan961117@gmail.com>
2018-02-18 14:35:39 +0000

    [Cryptle] Move utility classes to utility.py

--- cryptle/utility.py
+++ cryptle/utility.py
@@ -3,7 +3,6 @@ import json
 from datetime import datetime
 
 
-# @HARDCODE @REGRESSION @TEMPORARY
 def appendTimestamp(msg, t):
     return '{} At: {}'.format(msg, datetime.fromtimestamp(t).strftime("%d %H:%M:%S"))
 
@@ -40,4 +39,82 @@ def unpackTick(tick):
     action = 1 - tick['type'] * 2
 
     return price, timestamp, volume, action
+
+
+class DirectionFlag:
+    '''A boolean which can be accessed with specialised named attritubes.
+
+    The value of this flag can be accessed through members, up/down. The value
+    of these attributes are always oppositive of each other.
+    '''
+
+    def __init__(self):
+        self._up = self._down = False
+
+    @property
+    def up(self):
+        return self._up
+
+    @property
+    def down(self):
+        return self._down
+
+    @up.setter
+    def up(self, value):
+        self._up = value
+        self._down = not value
+
+    @down.setter
+    def down(self, value):
+        self._down = value
+        self._up = not value
+
+
+class IntensityFlag():
+
+    def __init__(self):
+        self._high = self._low = False
+
+    def setHigh(self):
+        self._high = True
+        self._low = False
+
+    def setLow(self):
+        self._high = False
+        self._low = True
+
+    @property
+    def high(self):
+        return self._high
+
+    @property
+    def low(self):
+        return self._low
+
+
+class TradeFlag:
+    '''A boolean which can be accessed with specialised named attritubes.
+
+    The value of this flag can be accessed through members, buy/sell. The value
+    of these attributes are always oppositive of each other.
+    '''
+
+    def __init__(self):
+        self._buy = self._sell = False
+
+    def setBuy(self):
+        self._buy = True
+        self._sell = False
+
+    def setSell(self):
+        self._buy = False
+        self._sell = True
+
+    @property
+    def buy(self):
+        return self._buy
+
+    @property
+    def sell(self):
+        return self._sell
 
