0e2aec5e9134e436692a2f176ac944ff5c5a9900
pinealan <achan961117@gmail.com>
2018-02-26 19:47:25 +0000

    [Cryptle] Fix Runtime, remove logger arg

--- cryptle/runtime.py
+++ cryptle/runtime.py
@@ -20,7 +20,6 @@ class Runtime:
     def __init__(self,
             strat,
             exchange,
-            logger,
             reporting_time=60,
             interval=1,
             istream=sys.stdin,
@@ -29,7 +28,6 @@ class Runtime:
         self.strat = strat
         self.port = strat.portfolio
         self.exchange = exchange
-        self.logger = logger
         self.reporting_time = reporting_time
         self.interval = interval
         self._input = istream
@@ -40,33 +38,29 @@ class Runtime:
         self.base_currency = self.strat.base_currency
 
 
-    def handle_input(self, line):
-        '''Handle commands. Return string representation command result'''
-        line = line.strip()
 
-        if line == 'h':
-            return self.help_text
-        elif line == 'r':
-            p = self.port
-            self._report()
-            s = (
-                'Equity:  {}\n'
-                'Cash:    {}\n'
-                'Balance: {}\n'
-            )
-            return s.format(p.equity, p.cash, p.balance)
-        elif line == 'l':
-            return pprint.pformat(list(self.strat.__dict__.keys()), indent=4)
-        elif line == 'q' or line == 'quit' or line == 'exit':
-            raise KeyboardInterrupt
-        else:
-            try:
-                return self.strat.__dict__[line]
-            except KeyError:
-                return 'Attribute does not exist'
+    def run_forever(self):
+        report_thread = threading.Thread(target=self._report_loop)
+        report_thread.start()
+
+        self._output(self.help_text)
+        self._report()
+
+        try:
+            for line in self._input:
+                if line:
+                    s = self._handle_input(line)
+                    self._output(s)
+        except KeyboardInterrupt:
+            self._output('Received termination request')
+        except Exception as e:
+            self._output('Caught unhandled exception {}'.format(e))
+        finally:
+            self._output('Terminating main loop...')
+            self._terminated = True
 
 
-    def report_loop(self):
+    def _report_loop(self):
         s = 0
         while not self._terminated:
             time.sleep(self.interval)
@@ -84,30 +78,37 @@ class Runtime:
                 pass
 
 
-    def init(self):
-        self._output(help_text)
-        self.logger.debug('Reporting started')
-        self._report()
+    def _handle_input(self, line):
+        '''Return string result of processed commaned'''
 
+        line = line.strip()
 
-    def run_forever(self):
-        report_thread = threading.Thread(target=self.report_loop)
-        report_thread.start()
-        try:
-            for line in self._input:
-                if line:
-                    s = self.handle_input(line)
-                    self._output(s)
-        except KeyboardInterrupt:
-            self._output('Received termination request')
-        except Exception as e:
-            self._output('Caught unhandled exception {}'.format(e))
-        finally:
-            self._output('Terminating main loop...')
-            self._terminated = True
+        if line == 'h':
+            return self.help_text
+        elif line == 'r':
+            p = self.port
+            self._report()
+            return s.format(p.equity, p.cash, p.balance)
+        elif line == 'l':
+            return pprint.pformat(list(self.strat.__dict__.keys()), indent=4)
+        elif line == 'q' or line == 'quit' or line == 'exit':
+            raise KeyboardInterrupt
+        else:
+            try:
+                return self.strat.__dict__[line]
+            except KeyError:
+                return 'Attribute does not exist'
 
 
     def _report(self):
-            self.logger.report('Equity:  {}'.format(self.port.equity))
-            self.logger.report('Cash:    {}'.format(self.port.cash))
-            self.logger.report('Balance: {}'.format(self.port.balance))
+        self._output(self._get_report_string())
+
+
+    def _get_report_string(self):
+        s = (
+            'Equity:  {}\n'
+            'Cash:    {}\n'
+            'Balance: {}\n'
+        )
+        s.format(self.port.equity, self.port.cash, self.port.balance)
+        return s
