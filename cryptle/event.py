6853eba86664ecfdf3f3062811c43dd7f807ed04
Alan Chan <achan961117@gmail.com>
2018-09-12 02:16:50 +0800

    Add execption and multi-event callback
    Add 4 exceptions at event module top level namespace
    Allow emitters to emit to multiple loops
    Allow callbacks to listen for multiple events

--- cryptle/event.py
+++ cryptle/event.py
@@ -2,6 +2,26 @@ import inspect
 import functools
 
 
+class LoopException(Exception):
+    pass
+
+
+class ExtraEmit(LoopException):
+    pass
+
+
+class NotListenedWarning(LoopException):
+    pass
+
+
+class UnboundedEmitter(LoopException):
+    pass
+
+
+class NoRegisteredMethod(LoopException):
+    pass
+
+
 class BaseLoop:
     def __init__(self):
         self._callbacks = {}
@@ -14,40 +34,59 @@ class BaseLoop:
 
     def emit(self, evt, data):
         """Emit events directly into the event loop."""
+
         if evt not in self._callbacks:
-            # @todo logging warn null callbacks
-            # @todo create user defined warning
-            raise Warning('No callbacks for event')
-            return
+            raise NotListenedWarning('No registered callbacks in this loop.')
+
         for cb in self._callbacks[evt]:
             cb(data)
 
 
 class Loop(BaseLoop):
     """Event loop middleware."""
-    def bind(self, instance):
-        """Binds an instance object and register it's decorated methods in the loop.
+
+    def bind(self, obj):
+        """Binds an obj object and register it's decorated methods in the loop.
+
+        Args:
+            obj: An instance from a class with methods decorated as callback or
+                emitters.
         """
-        for _, attr in inspect.getmembers(instance):
+        no_decorated = True
+        for _, attr in inspect.getmembers(obj):
             if inspect.ismethod(attr):
-                meth = attr
-                if getattr(meth, '_iscallback', False):
-                    self.on(meth._event, meth)
-                if getattr(meth, '_isemitter', False):
-                    instance._loop = self
+                method = attr
+                try:
+                    for evt in method._events:
+                        self.on(evt, method)
+                except AttributeError:
+                    pass
+                else:
+                    no_decorated = False
+
+                # Common use case is binding instance to single loop
+                # This optimise for performance
+                if hasattr(method, '_emits'):
+                    method._loops.append(self)
+                    no_decorated = False
+
+        if no_decorated:
+            raise NoRegisteredMethod('Object {} has no callbacks or emitters.'.foramt(obj))
 
 
-def on(event):
+def on(*events):
     """Decorator to designate object methods as event callbacks.
 
     The decorated method must take a single argument for the event data.
 
-    Todo: Multiple events can be listened to, either with multiple decorating
-        statement or as extra arguments a single decorate statment.
+    Multiple events can be listened to, either with multiple decorating statement
+    or as extra arguments a single decorate statment.
     """
     def decorator(method):
-        method._iscallback = True
-        method._event = event
+        if hasattr(method, '_events'):
+            method._events += events
+        else:
+            method._events = events
         return method
     return decorator
 
@@ -58,19 +97,31 @@ def emit(event):
     If the object instance of the decorated method is binded to a loop, the
     return value will be sent to the loop before being returned. The method must
     therefore return only a single value.
+
+    A method can only emit one event.
     """
     def decorator(method):
+        if getattr(method, '_emits', False):
+            raise ExtraEmitError('Only one event allowed for each method.')
+
         @functools.wraps(method)
         def wrapper(self, *args, **kwargs):
             rvalue = method(self, *args, **kwargs)
+            meth = getattr(self, method.__name__)
 
             # Bounded to an event loop instance
-            if getattr(self, '_loop', False):
-                self._loop.emit(event, rvalue)
+            try:
+                loops = meth._loops
+            except AttributeError:
+                # Todo: add setting to raise warning
+                loops = []
+
+            for loop in loops:
+                loop.emit(event, rvalue)
 
             return rvalue
 
-        wrapper._isemitter = True
-        wrapper._event = event
+        wrapper._emits = event
+        wrapper._loops = []
         return wrapper
     return decorator
