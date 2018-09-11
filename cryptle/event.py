c6c111138b56d66f68b80756c186cad17a16e4e3
Alan Chan <achan961117@gmail.com>
2018-09-11 11:48:39 +0800

    Add event.emit decorator

--- cryptle/event.py
+++ cryptle/event.py
@@ -1,4 +1,5 @@
 import inspect
+import functools
 
 
 class BaseLoop:
@@ -29,6 +30,8 @@ class Loop(BaseLoop):
                 meth = attr
                 if getattr(meth, '_iscallback', False):
                     self.on(meth._event, meth)
+                if getattr(meth, '_isemitter', False):
+                    instance._loop = self
 
 
 def on(event):
@@ -41,11 +44,18 @@ def on(event):
     return decorator
 
 
-# Todo
-class Emitter:
-    pass
-
-
-# Todo
 def emit(event):
-    pass
+    def decorator(method):
+        @functools.wraps(method)
+        def wrapper(self, *args, **kwargs):
+            # Unbounded
+            if getattr(self, '_loop', False):
+                self._loop.emit(event, method(self, *args, **kwargs))
+            # Bounded to an event loop instance
+            else:
+                return method(self, *args, **kwargs)
+
+        wrapper._isemitter = True
+        wrapper._event = event
+        return wrapper
+    return decorator
