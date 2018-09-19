ae037610ce6362cc29424ea2e6cabc1e3eca399e
Alan Chan <achan961117@gmail.com>
2018-09-19 20:12:02 +0800

    Add datafeed.connect()

--- cryptle/datafeed/__init__.py
+++ cryptle/datafeed/__init__.py
@@ -1,3 +1,30 @@
+import contextlib
+
+import cryptle.event as event
 from .bitstamp import BitstampFeed
 from .bitfinex import BitfinexFeed
 from .exception import *
+
+
+# Module level constants for supported datafeeds
+BITSTAMP = 'bitstamp'
+BITFINEX = 'bitfinex'
+
+
+@contextlib.contextmanager
+def connect(feed_name, *args, **kwargs):
+    """Datafeed as context manager."""
+    try:
+        if feed_name == BITSTAMP:
+            feed = BitstampFeed()
+            feed.connect(*args, **kwargs)
+
+        elif feed_name == BITFINEX:
+            feed = BitfinexFeed()
+            feed.connect(*args, **kwargs)
+
+        else:
+            raise ValueError('No datafeed named {}'.format(feed_name))
+        yield feed
+    finally:
+        feed.close()
