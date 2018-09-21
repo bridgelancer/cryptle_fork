0defb8bc8f527cb477ae976a8dc8112ea08e2dfe
Alan Chan <achan961117@gmail.com>
2018-09-22 01:51:19 +0800

    Remove support for Flask-like API

--- test/test_event.py
+++ test/test_event.py
@@ -1,6 +1,11 @@
 import inspect
-import cryptle.event as event
 import pytest
+
+import cryptle.event as event
+
+
+class CallbackReached(Exception):
+    pass
 
 
 def test_empty_emit():
@@ -9,31 +14,42 @@ def test_empty_emit():
         bus.emit('test_event', None)
 
 
-def test_on():
+def test_direct_emit():
+    def callback(data):
+        raise CallbackReached
+
+    bus = event.Bus()
+    bus.addListener('test_event', callback)
+    with pytest.raises(CallbackReached):
+        bus.emit('test_event', None)
+
+
+def test_global_on():
+    @event.on('test_event')
+    def callback(data):
+        assert data == None
+
+    bus = event.Bus()
+    bus.bind(callback)
+    bus.emit('test_event', None)
+
+
+def test_bound_bus_bound_callback():
     def callback(data):
         assert data == None
 
     evt = 'test_event'
     bus = event.Bus()
-    bus.on(evt, callback)
+    bus.addListener(evt, callback)
     assert evt in bus._callbacks
     assert len(bus._callbacks[evt]) == 1
 
 
-def test_callback_emit():
-    def callback(data):
-        assert data == None
-
-    bus = event.Bus()
-    bus.on('test_event', callback)
-    bus.emit('test_event', None)
-
-
-def test_unbound_source():
+def test_unbound_callback_method_unbound_bus():
     evt = 'test_event'
 
     class SMA:
-        @event.unbound_callback(evt)
+        @event.on(evt)
         def test(self, data):
             assert data == None
 
@@ -44,35 +60,53 @@ def test_unbound_source():
     assert len(bus._callbacks[evt]) == 1
 
 
-def test_emit_decorator():
+def test_unbound_source_method_unbound_bus():
     evt = 'test_event'
 
     class Tick:
-        @event.unbound_source(evt)
+        @event.source(evt)
         def test(self):
             return 1
 
     tick = Tick()
     bus = event.Bus()
     bus.bind(tick)
-    assert len(tick.test._buses) == 1
-    assert tick.test._buses[0] == bus
-    assert tick.test._emits
+    assert tick.test.buses[0] == bus
+    assert tick.test.event
 
     with pytest.raises(event.NotListenedWarning):
         assert tick.test() == 1
 
 
-def test_class_pair():
+def test_1_source_and_callback_instance():
     class Ticker:
-        @event.unbound_source('tick')
+        @event.source('tick')
+        def tick(self):
+            return 1
+
+        @event.on('tick')
+        def recv(self, data):
+            assert data == 1
+            raise CallbackReached
+
+    ticker = Ticker()
+    bus = event.Bus()
+    bus.bind(ticker)
+    with pytest.raises(CallbackReached):
+        ticker.tick()
+
+
+def test_1_source_instance_1_callback_instance():
+    class Ticker:
+        @event.source('tick')
         def tick(self, val=0):
             return val
 
     class Candle:
-        @event.unbound_callback('tick')
+        @event.on('tick')
         def recv(self, data, expect=0):
             assert data == expect
+            raise CallbackReached
 
     ticker = Ticker()
     candle = Candle()
@@ -81,15 +115,16 @@ def test_class_pair():
     bus.bind(ticker)
     bus.bind(candle)
 
-    ticker.tick()
+    with pytest.raises(CallbackReached):
+        ticker.tick()
 
 
-def test_multiple_bind():
+def test_1_callback_n_events():
     class Ticker:
         def __init__(self):
             self.called = 0
 
-        @event.unbound_callback('tick')
+        @event.on('tick')
         def print_tick(self, data):
             self.called += 1
             return data
@@ -98,22 +133,7 @@ def test_multiple_bind():
 
     ticker = Ticker()
     bus.bind(ticker)
-    bus.on('tick', ticker.print_tick)
+    bus.addListener('tick', ticker.print_tick)
 
     bus.emit('tick', 1)
     assert ticker.called == 2
-
-
-def test_direct_bus_binding_usage():
-    bus = event.Bus()
-    class Test:
-        @bus.on('test')
-        def foo(self, data):
-            assert data == 1
-
-        @bus.source('test')
-        def bar(self):
-            return 1
-
-    test_object = Test()
-    test_object.bar()
