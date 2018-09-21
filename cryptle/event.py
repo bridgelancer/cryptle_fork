e0f2ca00475af45aabef607296f2b2f8a576e425
Alan Chan <achan961117@gmail.com>
2018-09-22 01:51:19 +0800

    Remove support for Flask-like API

--- cryptle/event.py
+++ cryptle/event.py
@@ -19,11 +19,36 @@ class UnboundEmitter(BusException):
     pass
 
 
+class _Emitter:
+    def __init__(self, event, func):
+        if isinstance(func, self.__class__):
+            raise ExtraEmitError('An emitter may only emit one type of event.')
+        self.func = func
+        self.buses = []
+        self.event = event
+        self.instance = None
+
+    def __repr__(self):
+        return 'Emitter({}, {})'.format(self.event, self.func)
+
+    def __get__(self, instance, owner=None):
+        self.instance = instance
+        return self
+
+    def __call__(self, *args, **kwargs):
+        rvalue = self.func(self.instance)
+        for bus in self.buses:
+            bus.emit(self.event, rvalue)
+        return rvalue
+
+
 class Bus:
     """Event bus middleware.
 
     This class allows wiring of function chains to be decoupled into events.
 
+    Todo:
+        Allow removal of callback/emitter bindings.
     """
     def __init__(self):
         self._callbacks = defaultdict(list)
@@ -38,22 +63,23 @@ class Bus:
         for cb in self._callbacks[event]:
             cb(data)
 
-    def on(self, event, cb=None):
-        """Decorator for event callbacks. Can also be used inline.
+    def on(self, event):
+        """Decorator global functions as bounded event callbacks.
 
-        Multiple events can be listened to, either with multiple decorating statement
-        or as extra arguments a single decorate statment.
+        The same callback can listen to multiple events.
+
+        Warning:
+            Does not work on instance methods. This is because without the use
+            of metaclasses, bound methods do not inherit the template function
+            of a class.
         """
-        if cb:
-            self.addCallback(event, cb)
-        else:
-            def decorator(cb):
-                self.addCallback(event, cb)
-                return cb
-            return decorator
+        def decorator(func):
+            self.addListener(event, func)
+            return func
+        return decorator
 
     def source(self, event):
-        """Decorator for emitter function/methods.
+        """Decorator for global functions as bounded emitter function/methods.
 
         When the decorated method is binded to a bus, the functions's return value
         will be sent to the bus before being returned to the caller.
@@ -65,148 +91,105 @@ class Bus:
             In use cases with multiple buses, the order that the buses receive the
             event is in order that they were binded. This is an implementation
             detail and is subject to change. Multi-thread usage is thus discouraged.
+
+        Warning:
+            Does not work on instance methods. This is because without the use
+            of metaclasses, bound methods do not inherit the template function
+            of a class.
         """
-        def decorator(method):
-            wrapper = self.makeEmitter(event, method)
-            return wrapper
+        def decorator(func):
+            self.addEmitter(event, func)
+            return emitter
         return decorator
 
-    def addCallback(self, event, cb):
-        """Register events directly into the event bus."""
-        self._callbacks[event].append(cb)
+    def addListener(self, event, func):
+        self._callbacks[event].append(func)
 
-    def addEmitter(self, event, cb):
-        """Register emitter directly into the event bus."""
-        self._emitters[event].append(cb)
+    def addEmitter(self, event, func):
+        if isinstance(func, _Emitter):
+            if not func.event == event:
+                raise ExtraEmitError('An emitter may only emit one type of event.')
+            func.buses.append(self)
+            self._emitters[event].append(func)
+            return func
 
-    @staticmethod
-    def makeEmitter(event, method):
-        """Decorator for event emitting methods.
-
-        If the object instance of the decorated method is binded to a bus, the
-        return value will be sent to the bus before being returned. The method must
-        therefore return only a single value.
-
-        A method can only emit one event.
-        """
-        if getattr(method, '_emits', False):
-            raise ExtraEmitError('An emitter may only emit one type of event.')
-
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            rvalue = method(self, *args, **kwargs)
-            meth = getattr(self, method.__name__)
-
-            for bus in meth._buses:
-                bus.emit(event, rvalue)
-
-            return rvalue
-
-        wrapper._emits = event
-        wrapper._buses = []
-        return wrapper
+        emitter = _Emitter(event, func)
+        emitter.buses.append(self)
+        self._emitters[event].append(emitter)
+        return emitter
 
     def bind(self, object):
-        """Binds an object or function and register decorated methods on the bus.
+        """Catch all method for binding objects, methods, or functions to the bus.
+
+        Global functions or directly named instance methods will be bound as
+        normal. Instance objects will be inspected, and methods decorated as
+        source or callbacks will be bound.
 
         Args:
-            object: An object instance from a class with methods decorated as
-              callback or emitters.
+            object: The object to be bound/inspected.
 
         Note:
             Functors will not be treated as functions.
         """
-        no_decorated = True
-        if inspect.isfunction(object):
-            if hasattr(object, '_events'):
-                for evt in object._events:
-                    self.addCallback(evt, method)
-                no_decorated = False
+        decorated = False
 
-            if hasattr(object, '_emits'):
-                object._buses.append(self)
-                no_decorated = False
+        if hasattr(object, '_events'):
+            for evt in object._events:
+                self.addListener(evt, object)
+            decorated = True
 
-        # object likely is a class instance
-        else:
+        # global functions binded as emitters
+        if isinstance(object, _Emitter):
+            attr.buses.append(self)
+            decorated = True
+
+        # object is instance variable, find unbound methods in the instance
+        if inspect.isclass(object.__class__):
             for _, attr in inspect.getmembers(object):
-                if inspect.ismethod(attr):
-                    method = attr
 
-                    # Bind callbacks
-                    if hasattr(method, '_events'):
-                        for evt in method._events:
-                            self.addCallback(evt, method)
-                        no_decorated = False
+                # bind callbacks
+                if hasattr(attr, '_events'):
+                    for evt in attr._events:
+                        self.addListener(evt, attr)
+                    decorated = True
 
-                    # Bind emitters
-                    if hasattr(method, '_emits'):
-                        method._buses.append(self)
-                        no_decorated = False
+                # bind emitters
+                if isinstance(attr, _Emitter):
+                    attr.buses.append(self)
+                    decorated = True
 
-        if no_decorated:
-            raise ValueError('Object {} has no callbacks or emitters.'.foramt(object))
+        if not decorated:
+            raise ValueError('Object {} has no callbacks or emitters.'.format(object))
 
 
-def unbound_callback(*events):
-    """Decorator for unbounded event callbacks.
+def on(event):
+    """Unbound version of :meth:`Bus.on`.
 
     An unbounded callback must be binded to an event bus to be useful. Without
     a binding, the callback behaves just like a regular function.
     """
+    if not isinstance(event, str):
+        raise TypeError('Event string required')
+
     def decorator(method):
         if hasattr(method, '_events'):
-            method._events += events
+            method._events += event
         else:
-            method._events = events
+            method._events = [event]
         return method
     return decorator
 
 
-def unbound_source(event):
-    """Decorator for unbound event emitting instance methods.
+def source(event):
+    """Unbound version of :meth:`Bus.source`.
 
     An unbounded source must be binded to an event bus to be useful. Without
     a binding, the emitter behaves just like a regular function.
     """
+    if not isinstance(event, str):
+        raise TypeError('Event string required')
+
     def decorator(method):
-        if getattr(method, '_emits', False):
-            raise ExtraEmitError('An emitter may only emit one type of event.')
-
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            rvalue = method(self, *args, **kwargs)
-            meth = getattr(self, method.__name__)
-
-            for bus in meth._buses:
-                bus.emit(event, rvalue)
-
-            return rvalue
-
-        wrapper._emits = event
-        wrapper._buses = []
-        return wrapper
+        emitter = _Emitter(event, method)
+        return emitter
     return decorator
-
-def unbound_generic_source(event):
-    """Decorator for unbound event emitting functions/static methods.
-
-    An unbounded source must be binded to an event bus to be useful. Without
-    a binding, the emitter behaves just like a regular function.
-    """
-    def decorator(method):
-        if getattr(method, '_emits', False):
-            raise ExtraEmitError('An emitter may only emit one type of event.')
-
-        @functools.wraps(method)
-        def wrapper(*args, **kwargs):
-            rvalue = method(*args, **kwargs)
-
-            for bus in method._buses:
-                bus.emit(event, rvalue)
-
-            return rvalue
-
-        wrapper._emits = event
-        wrapper._buses = []
-        return wrapper
