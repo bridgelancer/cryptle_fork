e66122bf3ac11d410b4ae95ac35fe2711ef16974
Alan Chan <achan961117@gmail.com>
2018-10-02 00:33:55 +0800

    Add order filling with market trade event

--- cryptle/exchange/paper.py
+++ cryptle/exchange/paper.py
@@ -1,14 +1,34 @@
+"""Stub module for emulating an exchange for papertrading.
+
+The class Orderbook is a container for orders of a single instrument and
+provides abstraction to updating an orderbook.
+
+The class Paper is an higher level object that keeps track of available capital,
+handles incoming data and so on.
+
+Note:
+    Orderbook is treated as library code and Paper is treated as application
+    code. That means Orderbook only raises and never log whereas Paper only log
+    and never raise.
+"""
+
+
 import bisect
 import logging
 from collections import defaultdict
-from typing import Set, Tuple, List, Dict
+from typing import Set, Tuple, List, Dict, Union
 
 from cryptle.event import source, on
 from cryptle.orderbook import Orderbook
 from .exception import ExchangeError, OrderError
 
 
+# Module logger
 _LOG = logging.getLogger(__name__)
+
+
+# Type alias for compound return type in orderbook
+FilledOrders = Tuple[Set[int], Union[Tuple[int, float], None]]
 
 
 class Orderbook:
@@ -26,14 +46,16 @@ class Orderbook:
         self.ask_prices = []
 
         self._order_id = 0
-        self._open_orders = {}
 
-        self._bids = defaultdict(set)
-        self._asks = defaultdict(set)
+        # Open orders
+        self._bids = {}
+        self._asks = {}
+        self._bid_ids = defaultdict(set)
+        self._ask_ids = defaultdict(set)
 
     def __repr__(self):
-        return '<{}({} bids, {} asks)>'.format(
-            self.__qualname__,
+        return '<{}({} bid, {} ask)>'.format(
+            self.__class__.__qualname__,
             len(self._bids),
             len(self._asks),
         )
@@ -53,11 +75,11 @@ class Orderbook:
         except IndexError:
             pass
 
-        if price not in self._bids:
+        if price not in self._bid_ids:
             bisect.insort(self.bid_prices, price)
 
-        oid = self._new_order(amount, price)
-        self._bids[price].add(oid)
+        oid = self._new_bid(amount, price)
+        self._bid_ids[price].add(oid)
         return oid
 
     def create_ask(self, amount: float, price: float):
@@ -67,121 +89,179 @@ class Orderbook:
         except IndexError:
             pass
 
-        if price not in self._asks:
+        if price not in self._ask_ids:
             bisect.insort(self.ask_prices, price)
 
-        oid = self._new_order(amount, price)
-        self._asks[price].add(oid)
+        oid = self._new_ask(amount, price)
+        self._ask_ids[price].add(oid)
         return oid
 
-    def take_bid(self, amount: float) -> Tuple[Set[int], Tuple[int, float]]:
+    def fill_bid(self, amount: float, price: float) -> FilledOrders:
+        """Fill the bid orderbook based on a market sell event."""
+
         filled_orders = set()
-        filled_amount = 0
-        break_flag = False
-        partial_id = partial_filled = None
+        partial = None
+        for bid_price in self.bid_prices[::-1]:
+            if price > bid_price:
+                break
+            bids = self._subset_bids(self._bid_ids[bid_price])
+            fillable, partial, remainder = self._fillable_orders(bids, amount)
+            filled_orders.update(fillable)
+            if remainder == 0:
+                break
+            amount = remainder
+        for oid in filled_orders:
+            self.delete_bid(oid)
+        if partial:
+            oid, amount = partial
+            _, price = self._bids[oid]
+            self._bids[oid] = (amount, price)
 
-        # fill bids from highest to lowest price
+        return filled_orders, partial
+
+    def fill_ask(self, amount: float, price: float) -> FilledOrders:
+        """Fill the ask orderbook based on a market buy event."""
+
+        filled_orders = set()
+        partial = None
+        for ask_price in self.ask_prices:
+            if price < ask_price:
+                break
+            asks = self._subset_asks(self._ask_ids[ask_price])
+            fillable, partial, remainder = self._fillable_orders(asks, amount)
+            filled_orders.update(fillable)
+            if remainder == 0:
+                break
+        for oid in filled_orders:
+            self.delete_ask(oid)
+        if partial:
+            oid, amount = partial
+            _, price = self._bids[oid]
+            self._bids[oid] = (amount, price)
+        return filled_orders, partial
+
+    def fill_bid_by_price(self, price: float) -> FilledOrders:
+        """Fill the top bid orders up to provided price."""
+        raise NotImplementedError
+
+    def fill_ask_by_price(self, price: float) -> FilledOrders:
+        """Fill the top ask orders up to provided price."""
+        raise NotImplementedError
+
+    def fill_bid_by_amount(self, amount: float) -> FilledOrders:
+        """Fill the top bid orders up to provided amount."""
+
+        filled_orders = set()
+        partial = None
         for price in self.bid_prices[::-1]:
-            for oid in self._bids[price]:
-                order_amount, _ = self._open_orders[oid]
-                reminding_amount = amount - filled_amount
-
-                if reminding_amount == 0:
-                    break_flag = True
-                    break
-                elif reminding_amount >= order_amount:
-                    filled_amount += order_amount
-                    filled_orders.add(oid)
-                else:
-                    # filled all available orders
-                    partial_id = oid
-                    partial_filled = amount - filled_amount
-                    break_flag = True
-                    break
-            if break_flag:
+            bids = self._subset_bids(self._bid_ids[price])
+            fillable, partial, remainder = self._fillable_orders(bids, amount)
+            filled_orders.update(fillable)
+            if remainder == 0:
                 break
 
         # clean up filled orders
         for oid in filled_orders:
             self.delete_bid(oid)
 
-        # mark partially filled order
-        self._open_orders[partial_id] = (partial_filled, price)
+        # update partially filled order
+        if partial:
+            oid, amount = partial
+            _, price = self._bids[oid]
+            self._bids[oid] = (amount, price)
+        return filled_orders, partial
 
-        return filled_orders, (partial_id, partial_filled)
+    def fill_ask_by_amount(self, amount: float) -> FilledOrders:
+        """Fill the top ask orders up to provided amount."""
 
-    def take_ask(self, amount: float):
         filled_orders = set()
-        filled_amount = 0
-        break_flag = False
-        partial_id = partial_filled = None
-
-        # fill asks from highest to lowest price
-        for price in self.ask_prices[::-1]:
-            for oid in self._asks[price]:
-                order_amount, _ = self._open_orders[oid]
-                reminding_amount = amount - filled_amount
-
-                if reminding_amount == 0:
-                    break_flag = True
-                    break
-                elif reminding_amount >= order_amount:
-                    filled_amount += order_amount
-                    filled_orders.add(oid)
-                else:
-                    # filled all available orders
-                    partial_id = oid
-                    partial_filled = amount - filled_amount
-                    break_flag = True
-                    break
-            if break_flag:
+        partial = None
+        for price in self.ask_prices:
+            asks = self._subset_asks(self._ask_ids[price])
+            fillable, partial, remainder = self._fillable_orders(asks, amount)
+            filled_orders.update(fillable)
+            if remainder == 0:
                 break
 
         # clean up filled orders
         for oid in filled_orders:
             self.delete_ask(oid)
 
-        # mark partially filled order
-        self._open_orders[partial_id] = (partial_filled, price)
-
-        return filled_orders, (partial_id, partial_filled)
+        # update partially filled order
+        if partial:
+            oid, amount = partial
+            _, price = self._asks[oid]
+            self._asks[oid] = (amount, price)
+        return filled_orders, partial
 
     def delete_bid(self, oid):
-        if oid not in self._open_orders:
+        if oid not in self._bids:
             raise ValueError('No bid order of ID {}'.format(oid))
 
         # get order price
-        _, price = self._open_orders[oid]
-        self._bids[price].remove(oid)
+        _, price = self._bids[oid]
+        self._bid_ids[price].remove(oid)
 
-        # if this was the last order at it's price, remove empty set in self._bids
-        if len(self._bids[price]) == 0:
+        # if this was the last order at it's price, remove empty set in self._bid_ids
+        if len(self._bid_ids[price]) == 0:
             self.bid_prices.remove(price)
-            del self._bids[price]
+            del self._bid_ids[price]
 
         # finally reomve the order
-        del self._open_orders[oid]
+        del self._bids[oid]
 
     def delete_ask(self, oid):
-        if oid not in self._open_orders:
+        if oid not in self._asks:
             raise ValueError('No ask order of ID {}'.format(oid))
 
         # get order price
-        _, price = self._open_orders[oid]
-        self._asks[price].remove(oid)
+        _, price = self._asks[oid]
+        self._ask_ids[price].remove(oid)
 
-        # if this was the last order at it's price, remove empty set in self._asks
-        if len(self._asks[price]) == 0:
+        # if this was the last order at it's price, remove empty set in self._ask_ids
+        if len(self._ask_ids[price]) == 0:
             self.ask_prices.remove(price)
-            del self._asks[price]
+            del self._ask_ids[price]
 
         # finally reomve the order
-        del self._open_orders[oid]
+        del self._asks[oid]
 
-    def _new_order(self, amount, price):
+    def _new_bid(self, amount, price):
         self._order_id += 1
-        self._open_orders[self._order_id] = (amount, price)
+        self._bids[self._order_id] = (amount, price)
         return self._order_id
+
+    def _new_ask(self, amount, price):
+        self._order_id += 1
+        self._asks[self._order_id] = (amount, price)
+        return self._order_id
+
+    def _subset_bids(self, ids):
+        return {k: self._bids[k] for k in ids}
+
+    def _subset_asks(self, ids):
+        return {k: self._asks[k] for k in ids}
+
+    @staticmethod
+    def _fillable_orders(
+        orders: Dict[int, Tuple[float, float]],
+        amount: float
+    ) -> Tuple[Set[int], Union[Tuple[int, float], None], float]:
+        """Find orders to be filled given available amount."""
+        filled_orders = set()
+        partial = None
+
+        for oid, (order_amount, _) in orders.items():
+            if amount >= order_amount:
+                filled_orders.add(oid)
+            elif amount == 0:
+                break
+            else:
+                partial = (oid, amount)
+                amount = 0
+                break
+            amount -= order_amount
+        return filled_orders, partial, amount
 
 
 class Paper:
@@ -201,17 +281,43 @@ class Paper:
         self._last_price = 0
         self._last_time = 0
 
-    def update(self, pair: str, price: float, time=None):
+    def __repr__(self):
+        return "<{}(capital={})>".format(
+            self.__class__.__qualname__,
+            self.capital,
+        )
+
+    # Todo emit events for each filled order
+    def update(self, pair: str, amount: float, price: float, time=None):
         self._last_time  = time or self._last_time
         self._last_price = price
+        book = self._orderbooks[pair]
+
+        # use market buy/sell to do this part
+        try:
+            if price <= book.top_bid:
+                return book.fill_bid(amount, price)
+        except IndexError:
+            pass
+
+        try:
+            if price >= book.top_ask:
+                return book.fill_ask(amount, price)
+        except IndexError:
+            pass
+
+    def update_price(self, pair: str, price: float):
+        raise NotImplementedError
 
     def buy(self, pair, amount, price=None):
+        """Automatically decides whether to use market or limit order."""
         if price:
             return self.limitBuy(pair, amount, price)
         else:
             self.marketBuy(pair, amount, price)
 
     def sell(self, pair, amount, price=None):
+        """Automatically decides whether to use market or limit order."""
         if price:
             return self.limitSell(pair, amount, price)
         else:
@@ -236,15 +342,14 @@ class Paper:
         self.capital -= amount * exec_price
 
         _LOG.info('Limit buy {:7.5} {} ${:.5}', amount, pair.upper(), price)
-        _LOG.info('Paid {:.5} commission', _price * self.commission)
+        _LOG.info('Paid {:.5} commission', price * self.commission)
         return self._orderbooks[pair].create_bid(amount, price)
 
     def limitSell(self, pair, amount, price):
         exec_price = self._last_price * (1 - self.commission)
-        self.capital += amount * exec_price
 
         _LOG.info('Limit sell {:7.5} {} ${:.5}', amount, pair.upper(), price)
-        _LOG.info('Paid {:.5} commission', _price * self.commission)
+        _LOG.info('Paid {:.5} commission', price * self.commission)
         return self._orderbooks[pair].create_ask(amount, price)
 
     def stopBuy(self, pair, amount):
@@ -254,9 +359,6 @@ class Paper:
         NotImplementedError
 
     def cancelOrder(self, orderid):
-        NotImplementedError
-
-    def _matchOpenOrders(self):
         NotImplementedError
 
     @source('order:partialfill:paper')
--- test/test_paper.py
+++ test/test_paper.py
@@ -3,7 +3,7 @@ import pytest
 from cryptle.exchange.paper import Orderbook, Paper
 
 
-CAPTIAL = 100000.0
+CAPITAL = 100000.0
 PAIR   = 'btcusd'
 PRICE  = 1000.0
 AMOUNT = 100.0
@@ -13,29 +13,30 @@ def test_limit_buy():
     book = Orderbook()
 
     oid = book.create_bid(AMOUNT, PRICE)
-    filled, partial = book.take_bid(AMOUNT)
+    filled, partial = book.fill_bid_by_amount(AMOUNT)
 
-    assert partial == (None, None)
+    assert partial == None
     assert filled == {oid}
     assert len(book.bid_prices) == 0
     assert len(book.ask_prices) == 0
 
     oid = book.create_bid(AMOUNT, PRICE)
-    book.delete_bid(oid)
+    assert len(book.bid_prices) == 1
+    assert len(book._bids) == 1
 
+    book.delete_bid(oid)
     assert len(book.bid_prices) == 0
-    assert len(book.ask_prices) == 0
+    assert len(book._bids) == 0
 
 
 def test_limit_sell():
     book = Orderbook()
 
-    book.create_ask(AMOUNT, PRICE)
-    book.take_ask(AMOUNT)
-
     oid = book.create_ask(AMOUNT, PRICE)
-    book.delete_ask(oid)
+    filled, partial = book.fill_ask_by_amount(AMOUNT)
 
+    assert partial == None
+    assert filled == {oid}
     assert len(book.bid_prices) == 0
     assert len(book.ask_prices) == 0
 
@@ -52,11 +53,60 @@ def test_orderbook_raise_on_invalid_create():
         book.create_ask(AMOUNT, PRICE - 200)
 
 
+def test_orderbook_fill():
+    book = Orderbook()
+    bid_id = book.create_bid(AMOUNT, PRICE - 100)
+    ask_id = book.create_ask(AMOUNT, PRICE + 100)
+    assert len(book.bid_prices) == 1
+    assert len(book.ask_prices) == 1
+    assert len(book._bids) == 1
+    assert len(book._asks) == 1
+
+    # orderbook looks| bid: 900*100, ask: 1100*100 |
+    # sell comes in at 890*100, bid filled
+    filled, partial = book.fill_bid(AMOUNT, PRICE - 110)
+
+    assert filled == set((bid_id,))
+    assert partial == None
+    assert len(book.bid_prices) == 0
+    assert len(book._bids) == 0
+
+    # buy comes in at 1050*100, orderbook doesn't trigger fill
+    book.fill_ask(AMOUNT, PRICE + 50)
+    assert len(book._asks) == 1
+    assert book._asks[ask_id] == (AMOUNT, PRICE + 100)
+
+    # buy comes in at 1100*100, ask filled
+    book.fill_ask(AMOUNT, PRICE + 110)
+    assert len(book._asks) == 0
+
+
 def test_paper_market_order():
-    paper = Paper(CAPTIAL)
-    paper.update(PAIR, PRICE)
+    paper = Paper(CAPITAL)
+    paper.update(PAIR, AMOUNT, PRICE)
 
     paper.marketBuy(PAIR, AMOUNT)
-    assert paper.capital == CAPTIAL - PRICE * AMOUNT
+    assert paper.capital == CAPITAL - PRICE * AMOUNT
     paper.marketSell(PAIR, AMOUNT)
-    assert paper.capital == CAPTIAL
+    assert paper.capital == CAPITAL
+
+
+def test_paper_limit_order():
+    paper = Paper(CAPITAL)
+    paper.update(PAIR, AMOUNT, PRICE)
+    book = paper._orderbooks[PAIR]
+
+    paper.limitBuy(PAIR, AMOUNT, PRICE - 100)
+    paper.limitSell(PAIR, AMOUNT, PRICE + 100)
+
+    assert paper.capital == CAPITAL - PRICE * AMOUNT
+    assert len(book._bids) == 1
+    assert len(book._asks) == 1
+
+    paper.update(PAIR, AMOUNT, PRICE - 110)
+    assert len(book._bids) == 0
+    assert len(book._asks) == 1
+
+    paper.update(PAIR, AMOUNT, PRICE + 110)
+    assert len(book._bids) == 0
+    assert len(book._asks) == 0
