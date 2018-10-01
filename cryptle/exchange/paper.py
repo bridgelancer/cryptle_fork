2010dc0921ed4982daeb4069ec2dbf3638064dd5
Alan Chan <achan961117@gmail.com>
2018-10-01 08:59:40 +0800

    Add limit order to cryptle.paper.Orderbook

--- cryptle/exchange/paper.py
+++ cryptle/exchange/paper.py
@@ -1,6 +1,7 @@
 import bisect
 import logging
 from collections import defaultdict
+from typing import Set, Tuple, List, Dict
 
 from cryptle.event import source, on
 from cryptle.orderbook import Orderbook
@@ -29,7 +30,6 @@ class Orderbook:
 
         self._bids = defaultdict(set)
         self._asks = defaultdict(set)
-        self._orders = {}
 
     def __repr__(self):
         return '<{}({} bids, {} asks)>'.format(
@@ -38,63 +38,150 @@ class Orderbook:
             len(self._asks),
         )
 
-    def create_bid(self, amount: float, price: float):
-        if price > self._last_price:
-            raise ValueError('Bid price > top ask price')
+    @property
+    def top_bid(self):
+        return self.bid_prices[-1]
 
-        oid = self._new_order(price, amount)
-        if price not in self.bid_prices:
+    @property
+    def top_ask(self):
+        return self.ask_prices[0]
+
+    def create_bid(self, amount: float, price: float):
+        try:
+            if price > self.top_ask:
+                raise ValueError('Bid price > top ask price')
+        except IndexError:
+            pass
+
+        if price not in self._bids:
             bisect.insort(self.bid_prices, price)
-            self._bids[price].add(oid, )
-        else:
-            self._bids[price].add(amount)
+
+        oid = self._new_order(amount, price)
+        self._bids[price].add(oid)
+        return oid
 
     def create_ask(self, amount: float, price: float):
-        if price > self._last_price:
-            raise ValueError('Ask price < top bid price')
+        try:
+            if price < self.top_bid:
+                raise ValueError('Ask price < top bid price')
+        except IndexError:
+            pass
 
-        oid = self._new_order(price, amount)
-        if price not in self.ask_prices:
-            bisect.insort(self.ask_prices, price)
-            self._asks[price] = amount
-        else:
-            self._asks[price] += amount
-
-    def take_bid(self, amount: float, price: float):
-        if price not in self._bids:
-            raise ValueError('No existing bid order at requested price')
-        else:
-            self._bids[price] -= amount
-            if self._bids[price] < 0.0001:
-                self._bids.pop(price)
-                self.bid_prices.remove(price)
-
-    def take_ask(self, amount: float, price: float):
         if price not in self._asks:
-            raise ValueError('No existing ask order at requested price')
-        else:
-            self._asks[price] -= amount
-            if self._asks[price] < 0.0001:
-                self._asks.pop(price)
-                self.ask_prices.remove(price)
+            bisect.insort(self.ask_prices, price)
 
-    def delete_ask(self, amount: float, price: float):
-        NotImplementedError
-
-    def delete_ask(self, amount: float, price: float):
-        NotImplementedError
-
-    def take_bid_with_id(self, oid):
-        NotImplementedError
-
-    def take_ask_with_id(self, oid):
-        NotImplementedError
-
-    def _new_order(self, price, amount):
-        self._order_id += 1
-        oid = self._order_id
-        self._orders[oid] = (amount, price)
+        oid = self._new_order(amount, price)
+        self._asks[price].add(oid)
         return oid
+
+    def take_bid(self, amount: float) -> Tuple[Set[int], Tuple[int, float]]:
+        filled_orders = set()
+        filled_amount = 0
+        break_flag = False
+        partial_id = partial_filled = None
+
+        # fill bids from highest to lowest price
+        for price in self.bid_prices[::-1]:
+            for oid in self._bids[price]:
+                order_amount, _ = self._open_orders[oid]
+                reminding_amount = amount - filled_amount
+
+                if reminding_amount == 0:
+                    break_flag = True
+                    break
+                elif reminding_amount >= order_amount:
+                    filled_amount += order_amount
+                    filled_orders.add(oid)
+                else:
+                    # filled all available orders
+                    partial_id = oid
+                    partial_filled = amount - filled_amount
+                    break_flag = True
+                    break
+            if break_flag:
+                break
+
+        # clean up filled orders
+        for oid in filled_orders:
+            self.delete_bid(oid)
+
+        # mark partially filled order
+        self._open_orders[partial_id] = (partial_filled, price)
+
+        return filled_orders, (partial_id, partial_filled)
+
+    def take_ask(self, amount: float):
+        filled_orders = set()
+        filled_amount = 0
+        break_flag = False
+        partial_id = partial_filled = None
+
+        # fill asks from highest to lowest price
+        for price in self.ask_prices[::-1]:
+            for oid in self._asks[price]:
+                order_amount, _ = self._open_orders[oid]
+                reminding_amount = amount - filled_amount
+
+                if reminding_amount == 0:
+                    break_flag = True
+                    break
+                elif reminding_amount >= order_amount:
+                    filled_amount += order_amount
+                    filled_orders.add(oid)
+                else:
+                    # filled all available orders
+                    partial_id = oid
+                    partial_filled = amount - filled_amount
+                    break_flag = True
+                    break
+            if break_flag:
+                break
+
+        # clean up filled orders
+        for oid in filled_orders:
+            self.delete_ask(oid)
+
+        # mark partially filled order
+        self._open_orders[partial_id] = (partial_filled, price)
+
+        return filled_orders, (partial_id, partial_filled)
+
+    def delete_bid(self, oid):
+        if oid not in self._open_orders:
+            raise ValueError('No bid order of ID {}'.format(oid))
+
+        # get order price
+        _, price = self._open_orders[oid]
+        self._bids[price].remove(oid)
+
+        # if this was the last order at it's price, remove empty set in self._bids
+        if len(self._bids[price]) == 0:
+            self.bid_prices.remove(price)
+            del self._bids[price]
+
+        # finally reomve the order
+        del self._open_orders[oid]
+
+    def delete_ask(self, oid):
+        if oid not in self._open_orders:
+            raise ValueError('No ask order of ID {}'.format(oid))
+
+        # get order price
+        _, price = self._open_orders[oid]
+        self._asks[price].remove(oid)
+
+        # if this was the last order at it's price, remove empty set in self._asks
+        if len(self._asks[price]) == 0:
+            self.ask_prices.remove(price)
+            del self._asks[price]
+
+        # finally reomve the order
+        del self._open_orders[oid]
+
+    def _new_order(self, amount, price):
+        self._order_id += 1
+        self._open_orders[self._order_id] = (amount, price)
+        return self._order_id
 
 
 class Paper:
@@ -145,11 +232,17 @@ class Paper:
         _LOG.info('Paid {:.5} commission', self._last_price * self.commission)
 
     def limitBuy(self, pair, amount, price):
+        exec_price = self._last_price * (1 + self.commission)
+        self.capital -= amount * exec_price
+
         _LOG.info('Limit buy {:7.5} {} ${:.5}', amount, pair.upper(), price)
         _LOG.info('Paid {:.5} commission', _price * self.commission)
         return self._orderbooks[pair].create_bid(amount, price)
 
     def limitSell(self, pair, amount, price):
+        exec_price = self._last_price * (1 - self.commission)
+        self.capital += amount * exec_price
+
         _LOG.info('Limit sell {:7.5} {} ${:.5}', amount, pair.upper(), price)
         _LOG.info('Paid {:.5} commission', _price * self.commission)
         return self._orderbooks[pair].create_ask(amount, price)
@@ -166,7 +259,12 @@ class Paper:
     def _matchOpenOrders(self):
         NotImplementedError
 
-    @source('orderfilled:paper')
+    @source('order:partialfill:paper')
+    @staticmethod
+    def _announce_orderpartail(oid):
+        return (oid, amount)
+
+    @source('order:fill:paper')
     @staticmethod
     def _announce_orderfilled(oid):
         return oid
