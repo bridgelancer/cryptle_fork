cd4580832304527e3f4211865c0d43d61af6129f
Alan Chan <achan961117@gmail.com>
2018-10-01 08:59:40 +0800

    Add limit order to cryptle.paper.Orderbook

--- test/test_paper.py
+++ test/test_paper.py
@@ -1,3 +1,5 @@
+import pytest
+
 from cryptle.exchange.paper import Orderbook, Paper
 
 
@@ -9,26 +11,45 @@ AMOUNT = 100.0
 
 def test_limit_buy():
     book = Orderbook()
-    book.create_bid(AMOUNT, PRICE)
-    book.take_bid(AMOUNT, PRICE)
 
-
-def test_limit_buy_with_id():
-    book = Orderbook()
     oid = book.create_bid(AMOUNT, PRICE)
-    book.take_bid_with_id(oid)
+    filled, partial = book.take_bid(AMOUNT)
+
+    assert partial == (None, None)
+    assert filled == {oid}
+    assert len(book.bid_prices) == 0
+    assert len(book.ask_prices) == 0
+
+    oid = book.create_bid(AMOUNT, PRICE)
+    book.delete_bid(oid)
+
+    assert len(book.bid_prices) == 0
+    assert len(book.ask_prices) == 0
 
 
 def test_limit_sell():
     book = Orderbook()
+
     book.create_ask(AMOUNT, PRICE)
-    book.take_ask(AMOUNT, PRICE)
+    book.take_ask(AMOUNT)
 
-
-def test_limit_sell_with_id():
-    book = Orderbook()
     oid = book.create_ask(AMOUNT, PRICE)
-    book.take_ask_with_id(oid)
+    book.delete_ask(oid)
+
+    assert len(book.bid_prices) == 0
+    assert len(book.ask_prices) == 0
+
+
+def test_orderbook_raise_on_invalid_create():
+    book = Orderbook()
+    book.create_bid(AMOUNT, PRICE - 100)
+    book.create_ask(AMOUNT, PRICE + 100)
+
+    with pytest.raises(ValueError):
+        book.create_bid(AMOUNT, PRICE + 200)
+
+    with pytest.raises(ValueError):
+        book.create_ask(AMOUNT, PRICE - 200)
 
 
 def test_paper_market_order():
