29800c64f85f930d896260b1fac091d3b95b5391
simonysck <simonysck0117@gmail.com>
2018-10-28 02:27:10 +0800

    Added Pivot Point indicator

--- metric/timeseries/pivot.py
+++ metric/timeseries/pivot.py
@@ -1,125 +1,42 @@
 from metric.base import Timeseries
 
-class Pivot(Timeseries):
-    def __init__(self, high, low, close, lookback, n=5, list=False):
-        self._ts       = [high, low, close]
+class PivotPoints(Timeseries):
+    def __init__(self, timestamp, interval, high, low, close, days, n=8, list=False):
+        self._ts       = [timestamp, high, low, close]
         super().__init__(ts=self._ts)
-        self._lookback = lookback
+        self._days = days
+        self._lookback = (days*24*60*60) / interval     #the number of bars required to be cached
         self._cache    = []
-        self.value     = None
-        self.pp        = PP(self._ts, looback)
-
-        pp = self.pp
-        pphl = [pp, high, low]
-        self.r1        = R1([pp, low], lookback)
-        self.s1        = S1([pp, high], lookback)
-        # to consider how to encapsulate multiple RN/SN instances in pivot wrapper, now use lists to hold
-        self.rn        = [RN(pphl, lookback, n) for range(1:n+1)]
-        self.sn        = [SN(pphl, lookback, n) for range(1:n+1)]
+        self.period_high, self.period_low, self.period_close = None, None, None
+        self.pp = None
+        self.r = [None for i in range(n+1)]
+        self.s = [None for i in range(n+1)]
 
         if list:
             self.onList()
 
     @Timeseries.cache
     def evaluate(self):
-        self.broadcast()
+        if (timestamp + interval) % (24*60*60) == 0:    #Only update at specific time
+            #update the essential values of the previous period
+            self.period_high = max([i[1] for i in self._cache])
+            self.period_low = min([i[2] for i in self._cache])
+            self.period_close = self._cache[-1][3]
 
-    def onList(self):
-        pass
-
-# following pinescript documentation
-class PP:
-    def __init__(self, ts, lookback):
-        super().__init__(ts=self_ts)
-        self._ts       = ts
-        self._high     = ts[0]
-        self._low      = ts[1]
-        self._close    = ts[2]
-        self._lookback = lookback
-        self.value     = None
-
-    @Timeseries.cache
-    def evaluate(self):
-        self.value = (float(self._high) + float(self._low) + float(self._close))/ 3
-        self.broadcast()
-
-    def onList(self):
-        pass
-
-class R1:
-    def __init__(self, ts, lookback):
-        super().__init__(ts=self_ts)
-        self._ts       = ts
-        self._pp       = ts[0]
-        self._low      = ts[1]
-        self._lookback = lookback
-        self.value     = None
-
-    @Timeseries.cache
-    def evaluate(self):
-        self.value = (float(self._pp) + float(self._pp) + float(self._low))
-        self.broadcast()
-
-    def onList(self):
-        pass
-
-class S1:
-    def __init__(self, ts, lookback):
-        super().__init__(ts=self_ts)
-        self._ts       = ts
-        self._pp       = ts[0]
-        self._high     = ts[1]
-        self._lookback = lookback
-        self.value     = None
-
-    @Timeseries.cache
-    def evaluate(self):
-        self.value = (float(self._pp) - (float(self._high) - float(self._pp))
-        self.broadcast()
-
-    def onList(self):
-        pass
-
-class SN:
-    def __init__(self, ts, lookback, n):
-        if n < 2:
-            raise ValueError
-        super().__init__(ts=self_ts)
-        self.n         = n
-        self._ts       = ts
-        self._pp       = ts[0]
-        self._high     = ts[1]
-        self._low      = ts[2]
-        self._lookback = lookback
-        self.value     = None
-
-    @Timeseries.cache
-    def evaluate(self):
-        self.value = self._low - self.n * (self._high - self._pp)
-        self.broadcast()
+            # calculate pivot point, support and resistance levels
+            self.pp = (self.period_high + self.period_low + self.period_close) / 3
+            self.r[0] = self.s[0] = self.pp
+            self.r[1] = self.pp + (self.pp - self.period_low)
+            self.s[1] = self.pp - (self.period_high - self.pp)
+            for i in range(2,n+1):
+                self.r[i] = self.period_high + (i-1) * (self.pp - self.period_low)
+                self.s[i] = self.period_low - (i-1) * (self.period_high - self.pp)
 
     def onList(self):
         pass
 
 
-class RN:
-    def __init__(self, ts, lookback, n):
-        if n < 2:
-            raise ValueError
-        super().__init__(ts=self_ts)
-        self.n         = n
-        self._ts       = ts
-        self._pp       = ts[0]
-        self._high     = ts[1]
-        self._low      = ts[2]
-        self._lookback = lookback
-        self.value     = None
 
-    @Timeseries.cache
-    def evaluate(self):
-        self.value = self._high + self.n * (self._pp - self._low)
-        self.broadcast()
 
-    def onList(self):
-        pass
+
 
