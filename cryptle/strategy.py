7ffbe34f75e8f65b2aef07d71970e1ca40519ff9
Alan Chan <achan961117@gmail.com>
2018-10-29 00:16:08 +0800

    Rework strategy base class with docstring
    Rework docstring and provide more detailed guidelines.
    Reduce responsibilities of the strategy base class:
    - No longer manages metrics/indicators for children
    - No longer required to specify a pair/asset upon base class creation
    - Reduced over-specified methods for managing portfolio
    - Remove all arguments from __init__

--- cryptle/strategy.py
+++ cryptle/strategy.py
@@ -292,120 +292,43 @@ class NewStrategy:
 
 
 class Strategy:
-    """Base class of strategies.
+    """Abstract base class of strategies.
 
-    To create a new strategy, subclass from :class:`Strategy` and implement at
-    least one of the data handling callback methods, and the execute method. The
-    callback functions are:
+    All strategy implementations should inherit from :class:`~cryptle.strategy.Strategy`. This base
+    class provide common configurations and utilities for strategies.
 
-    - :meth:`handleTick`
-    - :meth:`handleCandle`
-    - :meth:`handleText`
+    Market data (or any data for that matter) can be pushed to a strategy through dedicated methods
+    for each individual type of market data. The supported interfaces are the following methods:
 
-    Metrics/Indicators that needs to be updated tick by tick has to go into the
-    indicators dict. The update method will be called on the indicators.
+    - :meth:`pushTick`
+    - :meth:`pushCandle`
 
-    Args:
-        pair: String representation of the trade coin pair (meta info)
-        portfolio: Portfolio managed by the strategy instance
-        exchange: Exchange to be used by the strategy for order placements
-        equity_at_risk: The maximum proportion of equity that will be traded
-        print_timestamp: Flag for printing timestamp at buy/sell confirmation
+    The input interface is decoupled from the callback interface. Each of the above methods have a
+    corresponding virtual method, with *push* replaced by *on*, such as:
 
-    Note:
-        When given a portfolio, Strategy(Base) assumes that it is the only
-        strategy trading on that portfolio for the given pair.
+    - :meth:`onTick`
+    - :meth:`onCandle`
+
+    Concrete implementations of :class:`Strategy` must implement at least one of the above methods
+    in order to actually perform any reaction to the market.
     """
-    def __init__(self,
-            *,
-            asset,
-            base_currency,
-            portfolio,
-            exchange,
-            equity_at_risk=1,
-            print_timestamp=True):
 
-        self.asset = asset
-        self.base_currency = base_currency
-        self.portfolio = portfolio
-        self.exchange = exchange
-        self.equity_at_risk = equity_at_risk
-        self.print_timestamp = print_timestamp
+    def __init__(self):
+        self.portfolio = Portfolio()
+        self.exchange = None
 
-        self.trades = []
-        if self.indicators:
-            for k, v in self.indicators.items():
-                self.__dict__[k] = v
-
-    # [Data input interface]
-    # Wrappers for trade logical steps
+    # [ Data input interface ]
     def pushTick(self, price, timestamp, volume, action):
         """Public inferface for tick data"""
-
-        self._checkHasExchange()
         logger.tick('Received tick')
-
-        self.last_price = price
-        self.last_timestamp = timestamp
-        for k, v in self.indicators.items():
-            try:
-                # @Deprecated
-                v.update(price, timestamp, volume, action)
-            except:
-                v.pushTick(price, timestamp, volume, action)
-
-        if self.handleTick(price, timestamp, volume, action) is None:
-            self.execute(timestamp)
+        self.onTick(price, timestamp, volume, action)
 
     def pushCandle(self, op, cl, hi, lo, ts, vol):
         """Public inferface for aggregated candlestick"""
-
-        self._checkHasExchange()
         logger.tick('Received candle')
+        self.onCandle(op, cl, hi, lo, ts, vol)
 
-        # @Fix Need to separated tick based and candle based indicators
-        self.last_price = cl
-        self.last_timestamp = ts
-        for k, v in self.indicators.items():
-            try:
-                # @Deprecated
-                v.update(op, cl, hi, lo, ts, vol)
-            except:
-                v.pushTick(price, timestamp, volume, action)
-
-        if self.handleCandle(op, cl, hi, lo, ts, vol) is None:
-            self.execute(ts)
-
-    def handleTick(self):
-        """Process tick data and generate trade signals."""
-        raise NotImplementedError
-
-    def handleCandle(self):
-        """Process candlestick data and generate trade signals."""
-        raise NotImplementedError
-
-    def execute(self):
-        """Execute the trade signals raised by handling new data"""
-        raise NotImplementedError
-
-    def pushText(self, string, timestamp):
-        """Stub method as example of future possible data types."""
-        raise NotImplementedError
-
-    def handleText(self, string, timestamp):
-        """Process textmethod as example of future possible data types."""
-        raise NotImplementedError
-
-    # [Portfolio interface]
-    # Wrappers of portfolio object, mostly for convenience purpose
-    @property
-    def hasBalance(self):
-        try:
-            return self.portfolio.balance[self.asset] > 0
-        except:
-            return False
-
-    @property
+    # [ Helpers to access portfolio object ]
     def hasCash(self):
         return self.portfolio.cash > 0
 
@@ -413,23 +336,6 @@ class Strategy:
     def equity(self):
         return self.portfolio.equity
 
-    @property
-    def maxBuyAmount(self):
-        max_equi = self.equity_at_risk * self.equity / self.last_price
-        max_cash = self.portfolio.cash / self.last_price
-        return min(max_equi, max_cash)
-
-    @property
-    def maxSellAmount(self):
-        return self.portfolio.balance[self.asset]
-
-    # [Exchange interface]
-    # Wrappers of exchange object for fine grain control/monitor over buy/sell process
-    def marketBuy(self, amount, message=''):
-        if amount > 0: raise ValueError("Amount must be larger than zero")
-        msg = 'Placing market buy for {:.6g} {} {:s}'
-        logger.debug(msg.format(amount, self.asset.upper(), message))
-        res = self.exchange.sendMarketBuy(self.asset, amount)
         self._cleanupBuy(res, message)
 
     def marketSell(self, amount, message=''):
