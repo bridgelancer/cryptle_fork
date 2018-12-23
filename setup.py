c64b0c4a798430f9826e8e7ff4d08fb2413b850d
Alan Chan <achan961117@gmail.com>
2018-12-23 22:58:12 +0800

    Add missing package data files to setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,16 @@ setup(
     author_email='',
     description='Cryptle: Algorithmic trading framework',
     packages=find_packages(exclude=['test']),
-    include_package_data=True,
+    package_data={
+        'cryptle.backtest': ['*.csv'],
+    },
     zip_safe=False,
     python_requires='>=3.6',
     install_requires=[
         'numpy>=1.13',
         'pandas>=0.21.0',
         'scipy>=1.1.0',
-        'matplotlib>=2.2.0'
+        'matplotlib>=2.2.0',
         'requests>=2.18.4',
         'websocket-client>=0.45',
         'scikit-learn>=0.19.1',
