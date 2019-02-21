4b344f64b5f72bd6d64937d62a0a1ddb3c62fc05
bridgelancer <thchoy@hotmail.com>
2019-02-21 16:15:11 +0800

    Changes in update_status.py

--- cryptle/metric/update_status.py
+++ cryptle/metric/update_status.py
@@ -1,19 +1,14 @@
 """Class for processing broadcasts in Timeseries.
 
-This module provides fine grain control as to the processing of broadcasts of the
-Timeseries objects.
-
-In addition to the naive approach of updating Timeseries cache whenever a non-zero value
-is returned from a GenericTS / Timeseries evaluate function, current tools include the
-use of the MemoryTS.execute decorator in GenericTS. By passing a Timestamp object to the
-last argument of the eval_func of a GenericTS, we could make control updating of
-Timeseries.
+This module implements UpdateStatus, which interprets the graph status in order to
+determine the broadcasts of the Timeseries objects, and interfaces with the Timeseries
+base class.
 
 A Timeseries can opt for using UpdateStatus as one of the methods of controlling the
 constraints of broadcasting and updating. The information of each TS subscriber would be
 obtained from the construction of this class. If no additional information was provided
 for updating, the naive approach would be used. The Timeseries concerned would suspend
-the update cascade until receiving all the broadcast signals from its subscriber.
+the updating cascade until receiving all the broadcast signals from its subscriber.
 
 Several modes of specifying update constraint has been encountered during usage. One
 would be requiring all the timeseries to update within the same update episode in order
@@ -22,9 +17,6 @@ update, in case the Timeseries was timestamped. Yet another possible mode of con
 would be specifying the number of broadcasts received for each subscriber before proceed
 to updating.
 
-This module intends to be an abstraction that allows effective implementation of all
-these modes of updating and allows easy extension of any mode of processing broadcasts in
-Timeseries in the future.
 
 """
 
@@ -37,14 +29,37 @@ logger = logging.getLogger(__name__)
 
 
 class UpdateStatus:
+    """Class for handling TSGraph information and interfacing with Timeseries base
+    class.
+
+    This module intends to be an abstraction that allows effective implementation
+    of all these modes of updating and allows easy extension of any mode of
+    processing broadcasts in Timeseries in the future.
+
+    Args
+    ----
+    ts: Timeseries
+        The actual concrete Timeseries object using the service of the graph.
+    update_mode : str
+        Choice of updating mode, can be 'naive', 'concurrnet', 'daily'.
+    publishers: list
+        List of publishers that this Timeseries listens to.
+
+    """
+
     def __init__(self, ts, update_mode: str, publishers):
         self.ts = ts
         self.update_mode = update_mode
+        # Consider reviewing - transition to the graph construct instead of using set
         self.publishers = publishers
         self.publishers_broadcasted = set()
 
-    def handleBroadcast(self, ts, pos):
-        """Public interface for TS to call.
+    def handleBroadcast(self, ts, pos) -> str:
+        """Public interface for Timeseries base class to call.
+
+        A ``status`` string of either 'hold' or 'clear' would be returned back to the
+        Timeseires base class caller. 'clear' would mean that the Timeseries is clear
+        for updating while 'hold' suspends the updating cascade.
 
         Args
         ----
@@ -68,6 +83,9 @@ class UpdateStatus:
             status = self.exectTimeHandling(ts, pos)
         else:
             raise NotImplementedError('UpdateStatus in progress...')
+
+        if status is None:
+            raise ValueError('Update status is not correctly handled.')
 
         return status
 
