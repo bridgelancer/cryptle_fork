a2f0844b1a432e7f1d29e4187cc78ad3d3b936c4
bridgelancer <thchoy@hotmail.com>
2019-02-24 21:38:01 +0800

    Remove unnecessary tests and changing names
    Note:
    -   Also blackened code in test suite

--- test/unit/test_aggregator.py
+++ test/unit/test_aggregator.py
@@ -1,7 +1,5 @@
 from cryptle.metric.timeseries.sma import SMA
-from cryptle.metric.timeseries.wma import WMA
 from cryptle.metric.timeseries.candle import CandleStick
-from cryptle.metric.timeseries.difference import Difference
 from cryptle.aggregator import DecoratedAggregator, Aggregator
 from cryptle.event import source, Bus
 
@@ -18,27 +16,15 @@ def pushTick(tick):
     return tick
 
 
-def pushAltQuad():
-    for i, price in enumerate(alt_quad):
-        pushTick([price, i, 0, 0])
-
-
 @pytest.fixture(scope='module')
-def data_pushing():
-    aggregator = DecoratedAggregator(5)
+def decorated_aggregator():
+    decorated_aggregator = DecoratedAggregator(5)
 
     bus = Bus()
     bus.bind(aggregator)
     bus.bind(pushTick)
 
-    return aggregator
-
-
-@pytest.fixture
-def primary_ts(data_pushing):
-    """Return the Timeseries objects with cache written for checking"""
-    sma, stick = data_pushing
-    return (sma, stick.o, stick.c, stick.h, stick.l, stick.v)
+    return decorated_aggregator
 
 
 def test_push_init_candle():
@@ -64,12 +50,12 @@ def test_push_tick():
             if bar is None:
                 assert len(aggregator._bars) == 1
             else:
-                assert bar == aggregator._bars[i//n - 1]._bar
+                assert bar == aggregator._bars[i // n - 1]._bar
                 assert len(aggregator._bars) == i // n + 1
 
-                assert aggregator.last_low == min(alt_quad[i - i%n:i+1])
-                assert aggregator.last_high == max(alt_quad[i - i%n:i+1])
-                assert aggregator.last_open == alt_quad[i- i%n]
+                assert aggregator.last_low == min(alt_quad[i - i % n : i + 1])
+                assert aggregator.last_high == max(alt_quad[i - i % n : i + 1])
+                assert aggregator.last_open == alt_quad[i - i % n]
                 assert aggregator.last_close == alt_quad[i]
 
 
@@ -78,33 +64,34 @@ def test_push_tick_with_empty_bars():
     q = 4
     aggregator = Aggregator(n)
 
-    alt_quad_shortened = alt_quad[::n*q]
+    alt_quad_shortened = alt_quad[:: n * q]
 
     for i, price in enumerate(alt_quad_shortened):
-        for num, bar in enumerate(aggregator.pushTick([price, i*n*q, 0, 0])):
+        for num, bar in enumerate(aggregator.pushTick([price, i * n * q, 0, 0])):
             if bar is None:
                 assert len(aggregator._bars) == 1
             else:
-                old_price = alt_quad_shortened[i-1]
-                if num != q-1:
-                    assert aggregator.last_open == alt_quad_shortened[i-1]
+                old_price = alt_quad_shortened[i - 1]
+                if num != q - 1:
+                    assert aggregator.last_open == alt_quad_shortened[i - 1]
                 else:
                     assert aggregator.last_open == alt_quad_shortened[i]
 
 
-def test_emit_aggregated_candle(data_pushing):
+def test_emit_aggregated_candle(decorated_aggregator):
     n = 5
     # getting the aggregator implementation
-    aggregator = data_pushing.aggregator
+    aggregator = decorated_aggregator.aggregator
 
     for i, price in enumerate(alt_quad):
         pushTick([price, i, 0, 0])
         assert len(aggregator._bars) == i // n + 1
 
-        assert aggregator.last_low == min(alt_quad[i - i%n:i+1])
-        assert aggregator.last_high == max(alt_quad[i - i%n:i+1])
-        assert aggregator.last_open == alt_quad[i- i%n]
+        assert aggregator.last_low == min(alt_quad[i - i % n : i + 1])
+        assert aggregator.last_high == max(alt_quad[i - i % n : i + 1])
+        assert aggregator.last_open == alt_quad[i - i % n]
         assert aggregator.last_close == alt_quad[i]
+
 
 @pytest.mark.skip(reason='not implemented')
 def test_emitAllMetrics():
