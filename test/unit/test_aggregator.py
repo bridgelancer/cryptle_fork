642f70924c4f738aceb2c182370b401fdf39612a
bridgelancer <thchoy@hotmail.com>
2019-02-24 20:58:31 +0800

    Add tests for testing implementation of Aggregator

--- test/unit/test_aggregator.py
+++ test/unit/test_aggregator.py
@@ -46,19 +46,54 @@ def primary_ts(data_pushing):
 
 
 def test_push_init_candle():
-    pass
+    """Testing the side effects of _pushInitCandle method of Aggregator"""
+    aggregator = Aggregator(1)
+    bar = aggregator._pushInitCandle(10, 1, 0, 0)
+    assert bar is None
+    assert len(aggregator._bars) == 1
+
+    # Always return second-last candle, the most recent finished bar
+    bar = aggregator._pushInitCandle(20, 1, 0, 0)
+    assert bar == aggregator._bars[0]._bar
+    assert len(aggregator._bars) == 2
 
 
-def test_push_full_candle():
-    pass
+def test_push_tick():
+    """Asserting aggregation correctness for each tick pushed"""
+    n = 5
+    aggregator = Aggregator(n)
+
+    for i, price in enumerate(alt_quad):
+        for bar in aggregator.pushTick([price, i, 0, 0]):
+            if bar is None:
+                assert len(aggregator._bars) == 1
+            else:
+                assert bar == aggregator._bars[i//n - 1]._bar
+                assert len(aggregator._bars) == i // n + 1
+
+                assert aggregator.last_low == min(alt_quad[i - i%n:i+1])
+                assert aggregator.last_high == max(alt_quad[i - i%n:i+1])
+                assert aggregator.last_open == alt_quad[i- i%n]
+                assert aggregator.last_close == alt_quad[i]
 
 
-def test_push_empty_candle():
-    pass
+def test_push_tick_with_empty_bars():
+    n = 5
+    q = 4
+    aggregator = Aggregator(n)
 
+    alt_quad_shortened = alt_quad[::n*q]
 
-def test_pushTick():
-    pass
+    for i, price in enumerate(alt_quad_shortened):
+        for num, bar in enumerate(aggregator.pushTick([price, i*n*q, 0, 0])):
+            if bar is None:
+                assert len(aggregator._bars) == 1
+            else:
+                old_price = alt_quad_shortened[i-1]
+                if num != q-1:
+                    assert aggregator.last_open == alt_quad_shortened[i-1]
+                else:
+                    assert aggregator.last_open == alt_quad_shortened[i]
 
 
 def test_emit_aggregated_candle():
